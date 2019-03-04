49e38bf44b280cf790d60e9e506708aa5b2a20b2
Simon Hofmann <simon.hofmann@consol.de>
2019-03-04 08:29:25 +0100

    Application class stub

--- packages/sakuli-legacy/src/context/common/application.class.ts
+++ packages/sakuli-legacy/src/context/common/application.class.ts
@@ -1,3 +1,28 @@
 export class Application {
-    
+    public open() {
+    }
+
+    public focus() {
+    }
+
+    public focusWindow(windowNumber: number) {
+    }
+
+    public close(optSilent?: boolean) {
+    }
+
+    public kill(optSilent?: boolean) {
+    }
+
+    public setSleepTime(seconds: number) {
+    }
+
+    public getRegion() {
+    }
+
+    public getRegionForWindow(windowNumber: number) {
+    }
+
+    public getName() {
+    }
 }
