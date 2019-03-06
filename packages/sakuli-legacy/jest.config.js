f56ef22a92acfe3362a0a975e8ffd8cdc13955c2
Simon Hofmann <simon.hofmann@consol.de>
2019-03-06 13:10:13 +0100

    Reverted rootDir in Jest config

--- packages/sakuli-legacy/jest.config.js
+++ packages/sakuli-legacy/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     testEnvironment: 'node',
     name: 'sakuli-legacy',
     displayName: 'sakuli-legacy',
-    rootDir: 'src/context/common',
+    rootDir: 'src/',
     testMatch: [
         `**/!(*.aix.*|*.darwin.*|*.freebsd.*|*.linux.*|*.openbsd.*|*.sunos.*|*.win32.*)+(spec|test).[jt]s?(x)`,
         `**/?(*.${process.platform}.)+(spec|test).[jt]s?(x)`
