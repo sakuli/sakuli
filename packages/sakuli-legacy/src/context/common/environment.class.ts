2f9f4aa76eb7783ff8cbdcb3e203a7a0075f46e9
Simon Hofmann <simon.hofmann@consol.de>
2019-03-25 23:15:00 +0100

    Added LoggingEnvironment

--- packages/sakuli-legacy/src/context/common/environment.class.ts
+++ packages/sakuli-legacy/src/context/common/environment.class.ts
@@ -1,16 +1,176 @@
 import {Key} from "./key.class";
 import {CommandLineResult} from "./commandline-result.class";
-import {keyboard, mouse, screen} from "@nut-tree/nut-js";
-import {parse} from "path";
-import {FileType} from "@nut-tree/nut-js/dist/lib/file-type.enum";
-import {cwd} from "process";
 import {decryptSecret} from "./secrets.function";
 import {ClipboardApi} from "./actions/clipboard.functions";
 import {KeyboardApi} from "./actions/keyboard.functions";
+import {TestExecutionContext} from "@sakuli/core";
+import {ScreenApi} from "./actions/screen.functions";
+import {MouseApi} from "./actions/mouse.functions";
+
+export function createEnvironmentClass(ctx: TestExecutionContext) {
+    return class LoggingEnvironment extends Environment {
+        constructor() {
+            super();
+        }
+
+        public getSimilarity(): number {
+            return super.getSimilarity();
+        }
+
+        public setSimilarity(similarity: number) {
+            super.setSimilarity(similarity);
+        }
+
+        public resetSimilarity() {
+            super.resetSimilarity();
+        }
+
+        public async takeScreenshot(filename: string): Promise<string> {
+            ctx.logger.info(`Taking screenshot with filename '${filename}'`);
+            return super.takeScreenshot(filename);
+        }
+
+        public async takeScreenshotWithTimestamp(filename: string): Promise<string> {
+            ctx.logger.info(`Taking screenshot with filename '${filename}' and timestamp`);
+            return super.takeScreenshotWithTimestamp(filename);
+        }
+
+        public async sleep(seconds: number): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Sleeping for ${seconds} seconds`);
+            return new Promise<LoggingEnvironment>(resolve => setTimeout(() => resolve(this), seconds * 1000));
+        }
+
+        public async sleepMs(milliseconds: number): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Sleeping for ${milliseconds} milliseconds`);
+            return new Promise<LoggingEnvironment>(resolve => setTimeout(() => resolve(this), milliseconds));
+        }
+
+        public async getRegionFromFocusedWindow() {
+            // TODO
+        }
+
+        public async getClipboard(): Promise<string> {
+            const content = super.getClipboard();
+            ctx.logger.info(`Clipboard content: '${content}'`);
+            return content;
+        }
+
+        public async setClipboard(text: string): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Setting clipboard content: '${text}'`);
+            return super.setClipboard(text);
+        }
+
+        public async pasteClipboard(): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Pasting native clipboard content`);
+            return super.pasteClipboard();
+        }
+
+        public async copyIntoClipboard(): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Copying to native clipboard`);
+            return super.copyIntoClipboard();
+        }
+
+        public async cleanClipboard(): Promise<void> {
+            // TODO
+        }
+
+        public async paste(text: string): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Pasting '${text}' via native clipboard`);
+            return super.paste(text);
+        }
+
+        public async pasteMasked(text: string): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Pasting '****' via native clipboard`);
+            return this.paste(text);
+        }
+
+        public async pasteAndDecrypt(text: string): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Pasting encrypted text '${text}' via native clipboard`);
+            return super.pasteAndDecrypt(text);
+        }
+
+        public async type(text: string, ...optModifiers: Key[]): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Typing text '${text}' via keyboard`);
+            return super.type(text, ...optModifiers);
+        }
+
+        public async typeMasked(text: string, ...optModifiers: Key[]): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Typing text '****' via keyboard`);
+            return super.type(text, ...optModifiers);
+        }
+
+        public async typeAndDecrypt(text: string, ...optModifiers: Key[]): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Typing encrypted text ${text}`);
+            return super.typeAndDecrypt(text, ...optModifiers);
+        }
+
+        public decryptSecret(secret: string): Promise<string> {
+            ctx.logger.info(`Decrypting secret '${secret}'`);
+            return super.decryptSecret(secret);
+        }
+
+        public async keyDown(...keys: Key[]): Promise<LoggingEnvironment> {
+            return super.keyDown(...keys);
+        }
+
+        public async keyUp(...keys: Key[]): Promise<LoggingEnvironment> {
+            return super.keyUp(...keys);
+        }
+
+        public async write(text: string): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Writing: ${text} via keyboard`);
+            return super.write(text);
+        }
+
+        public async mouseWheelDown(steps: number): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Scrolling down ${steps} px`);
+            return super.mouseWheelDown(steps);
+        }
+
+        public async mouseWheelUp(steps: number): Promise<LoggingEnvironment> {
+            ctx.logger.info(`Scrolling up ${steps} px`);
+            return super.mouseWheelUp(steps);
+        }
+
+        public isWindows(): boolean {
+            ctx.logger.info(`Is Windows?`);
+            return super.isWindows();
+        }
+
+        public isLinux(): boolean {
+            ctx.logger.info(`Is Linux?`);
+            return super.isLinux();
+        }
+
+        public isDarwin(): boolean {
+            ctx.logger.info(`Is Darwin?`);
+            return super.isDarwin();
+        }
+
+        public getOsIdentifier(): string {
+            ctx.logger.info(`Fetching OS identifier`);
+            return super.getOsIdentifier();
+        }
+
+        public runCommand(command: string, optThrowException: boolean = false): Promise<CommandLineResult> {
+            return new Promise<CommandLineResult>(resolve => resolve);
+        }
+
+        public getEnv(key: string): string | undefined {
+            ctx.logger.info(`Accessing environment variable '${key}'`);
+            return super.getEnv(key);
+        }
+
+        public getProperty(key: string): string | undefined {
+            // TODO
+            return;
+        }
+    }
+}
 
 export class Environment {
-    private static DEFAULT_SIMILARITY = 0.8;
-    private currentSimilarity = Environment.DEFAULT_SIMILARITY;
+    static DEFAULT_SIMILARITY = 0.99;
+    currentSimilarity = Environment.DEFAULT_SIMILARITY;
 
     constructor() {
     }
@@ -30,25 +190,19 @@ export class Environment {
     }
 
     public async takeScreenshot(filename: string): Promise<string> {
-        const pathParts = parse(filename);
-        const outputDir = (pathParts.dir && pathParts.dir.length > 0) ? pathParts.dir : cwd();
-        return screen.capture(pathParts.name, FileType.PNG, outputDir);
+        return ScreenApi.takeScreenshot(filename);
     }
 
     public async takeScreenshotWithTimestamp(filename: string): Promise<string> {
-        const pathParts = parse(filename);
-        const outputDir = (pathParts.dir && pathParts.dir.length > 0) ? pathParts.dir : cwd();
-        return screen.capture(pathParts.name, FileType.PNG, outputDir, "", `_${Date.now()}`);
+        return ScreenApi.takeScreenshotWithTimestamp(filename);
     }
 
     public async sleep(s: number): Promise<Environment> {
-        await new Promise<void>(resolve => setTimeout(resolve, s * 1000));
-        return this;
+        return new Promise<Environment>(resolve => setTimeout(() => resolve(this), s * 1000));
     }
 
     public async sleepMs(ms: number): Promise<Environment> {
-        await new Promise<void>(resolve => setTimeout(resolve, ms));
-        return this;
+        return new Promise<Environment>(resolve => setTimeout(() => resolve(this), ms));
     }
 
     public async getRegionFromFocusedWindow() {
@@ -84,7 +238,6 @@ export class Environment {
     }
 
     public async pasteMasked(text: string): Promise<Environment> {
-        // TODO Do not log text, once we log anything at all
         return this.paste(text);
     }
 
@@ -99,7 +252,6 @@ export class Environment {
     }
 
     public async typeMasked(text: string, ...optModifiers: Key[]): Promise<Environment> {
-        // TODO Do not log text, once we log anything at all
         return this.type(text, ...optModifiers);
     }
 
@@ -123,17 +275,17 @@ export class Environment {
     }
 
     public async write(text: string): Promise<Environment> {
-        await keyboard.type(text);
+        await KeyboardApi.type(text);
         return this;
     }
 
     public async mouseWheelDown(steps: number): Promise<Environment> {
-        await mouse.scrollDown(steps);
+        await MouseApi.scrollDown(steps);
         return this;
     }
 
     public async mouseWheelUp(steps: number): Promise<Environment> {
-        await mouse.scrollUp(steps);
+        await MouseApi.scrollUp(steps);
         return this;
     }
 
