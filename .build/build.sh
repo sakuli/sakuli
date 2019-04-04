db82fd59d9e900747be876357df12eec2b955194
Simon Hofmann <simon.hofmann@consol.de>
2019-04-04 13:20:37 +0200

    Added curl

--- .build/build.sh
+++ .build/build.sh
@@ -22,7 +22,8 @@ cd $targetDir
 echo "Installing node version $nodeVersion"
 nvm install $nodeVersion
 echo "Installing Docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-get install -y curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
