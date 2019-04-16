f471b6d04a879c03b06e128e580d5aa9e7577e17
Tim Keiner <keiner.tim@gmail.com>
2019-04-16 17:30:37 +0200

    remove testing utils and necessary dependencies

--- packages/sakuli-commons/src/index.ts
+++ packages/sakuli-commons/src/index.ts
@@ -1,6 +1,5 @@
 export * from './maybe';
 export * from './type.interface';
-export * from './testing';
 export * from './async';
 export * from './logger';
 export * from './properties'
--- packages/sakuli-commons/src/testing/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './fs-layout/mock-fs.function'
-export * from './fs-layout/fs-layout-entry.interface'
-export * from './fs-layout/file.function'
