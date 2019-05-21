e726ad776ebb7721504f218fcd400e302d60fb25
Simon Hofmann <simon.hofmann@consol.de>
2019-05-21 13:51:47 +0200

    (#75) Testcase indices now start at 1

--- packages/sakuli-legacy/src/context/common/test-case.class.ts
+++ packages/sakuli-legacy/src/context/common/test-case.class.ts
@@ -14,12 +14,12 @@ const getTestMetaData = (ctx: TestExecutionContext): TestMetaData => {
     const suiteName = ifPresent(ctx.getCurrentTestSuite(), suite => ifPresent(suite.id, id => id, () => "UNKNOWN_TESTSUITE"), () => "UNKNOWN_TESTSUITE");
     let caseName = ifPresent(ctx.getCurrentTestCase(), testCase => testCase.id, () => null);
     caseName = ifPresent(caseName, () => caseName, () => {
-        return ifPresent(ctx.getCurrentTestSuite(), ts => `testcase_${ts.testCases.length - 1}`, () => "testcase_0");
+        return ifPresent(ctx.getCurrentTestSuite(), ts => `testcase_${ts.testCases.length}`, () => "testcase_1");
     });
 
     return ({
         suiteName,
-        caseName: caseName || "testcase_0"
+        caseName: caseName || "testcase_1"
     });
 };
 
