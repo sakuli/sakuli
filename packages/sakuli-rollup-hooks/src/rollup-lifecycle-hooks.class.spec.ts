c8edb6d20835e534abba5a7ddaa562f67e068f88
Tim Keiner <keiner.tim@gmail.com>
2019-05-30 13:46:41 +0200

    typescript plugin loaded in rollup hook

--- packages/sakuli-rollup-hooks/src/rollup-lifecycle-hooks.class.spec.ts
+++ packages/sakuli-rollup-hooks/src/rollup-lifecycle-hooks.class.spec.ts
@@ -5,26 +5,27 @@ import {mockPartial} from "sneer";
 
 describe('RollupLifecycleHooks', () => {
 
+    let project: Project;
+    beforeEach(() => {
+        project = mockPartial<Project>({
+            rootDir: join(__dirname, '__mock__'),
+            testFiles: []
+        });
+    });
+
     it('should create a bundle from index.js file', async () => {
         const hooks = new RollupLifecycleHooks();
         const o = await hooks.readFileContent({
-            path: 'index.js'
-        }, mockPartial<Project>({
-            rootDir: join(__dirname, '__mock__'),
-            testFiles: []
-        }));
+            path: 'index.js',
+        }, project);
         expect(o.trim().startsWith("(function () {"));
         expect(o).toContain("const pi = 3.4");
         expect(o).toContain("console.log(pi)");
         return expect(o.trim().endsWith("}())"));
     });
 
-    it('should c', async () => {
+    it('should add native node modules as globals to context', async () => {
         const ctx = mockPartial<TestExecutionContext>({});
-        const project = mockPartial<Project>({
-            rootDir: join(__dirname, '__mock__'),
-            testFiles: []
-        });
         const hooks = new RollupLifecycleHooks();
         await hooks.readFileContent({
             path: 'with-native.js'
@@ -35,4 +36,35 @@ describe('RollupLifecycleHooks', () => {
             'fs': expect.anything()
         }))
     });
+
+    it('should add typescript plugin if file extension is .ts', async () => {
+        const hooks = new RollupLifecycleHooks();
+        await hooks.readFileContent({
+            path: 'ts.ts'
+        }, project);
+
+    });
+
+    it('should add native node modules as globals to context with typescript', async () => {
+        const ctx = mockPartial<TestExecutionContext>({});
+        const hooks = new RollupLifecycleHooks();
+        await hooks.readFileContent({
+            path: 'ts/index.ts'
+        }, project);
+
+        await expect(hooks.requestContext(ctx, project)).resolves.toEqual(expect.objectContaining({
+            'http': expect.anything(),
+            'fs': expect.anything()
+        }))
+    });
+
+    it('should error when file has typescript errors', async () => {
+        const hooks = new RollupLifecycleHooks();
+
+        await expect(hooks.readFileContent({
+            path: 'ts/error.ts'
+        }, project)).rejects.toEqual(expect.anything());
+
+    });
+
 });
--- packages/sakuli-rollup-hooks/src/rollup-lifecycle-hooks.class.ts
+++ packages/sakuli-rollup-hooks/src/rollup-lifecycle-hooks.class.ts
@@ -1,20 +1,33 @@
 import {Project, TestExecutionContext, TestExecutionLifecycleHooks} from "@sakuli/core";
 import {TestFile} from "@sakuli/core/dist/loader/model/test-file.interface";
-import {rollup} from "rollup";
-import {isAbsolute, join} from "path";
-
+import {Plugin, rollup} from "rollup";
+import rollupTsPlugin from 'rollup-plugin-typescript2';
+import {extname, isAbsolute, join} from "path";
 
 export class RollupLifecycleHooks implements TestExecutionLifecycleHooks {
 
     private imports: string[] = [];
     async readFileContent(file: TestFile, project: Project): Promise<string> {
-        const filePath =  isAbsolute(project.rootDir)
+        const filePath = isAbsolute(project.rootDir)
             ? join(project.rootDir, file.path)
             : join(process.cwd(), project.rootDir, file.path);
 
+        const plugins: Plugin[] = [];
+        const extName = extname(filePath);
+        if(extName === '.ts' || extName === '.tsx') {
+            plugins.push(rollupTsPlugin(<any>{
+                tsconfigOverride: {
+                    compilerOptions: {
+                        module: 'ESNext'
+                    }
+                }
+            }));
+        }
         const bundle = await rollup({
             input: filePath,
+            plugins
         });
+
         const {output} = await bundle.generate({
             format: 'iife',
             sourcemap: true,
@@ -22,6 +35,7 @@ export class RollupLifecycleHooks implements TestExecutionLifecycleHooks {
         });
         const [rollupOutput] = output;
         this.imports = rollupOutput.imports;
+        console.log(rollupOutput.code);
         return Promise.resolve(rollupOutput.code);
     }
 
