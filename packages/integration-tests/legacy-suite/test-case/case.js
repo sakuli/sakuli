4cb7aecd15407636f221671affe33ca56063fe5a
Tim Keiner <keiner.tim@gmail.com>
2019-06-27 16:52:58 +0200

    (#98) add to reader property to expectations

--- packages/integration-tests/legacy-suite/test-case/case.js
+++ packages/integration-tests/legacy-suite/test-case/case.js
@@ -1,15 +1,46 @@
 (async () => {
     const testCase = new TestCase();
     try {
-        await _navigateTo("https://sakuli.io");                                  // 1
-        testCase.endOfStep("Open Landing Page",5);             // 2
-        await _click(_link("Getting started"));                         // 3
-        testCase.endOfStep("Navigate to Getting Started",3);   // 4
-        await _highlight(_code("npm init"));                            // 5
-        testCase.endOfStep("Find npm init code sample");
+        await _navigateTo("https://try.gogs.io/");
+        testCase.endOfStep("Open Gogs Landing Page", 5, 10);
+        //await _click(_link("Sign In"));
+// await _click(_textbox("user_name"));
+        await _setValue(_textbox("user_name"), "gogs");
+        await _setValue(_password("password"), "gogs");
+        await _click(_submit("Sign In"));
+        testCase.endOfStep("Login", 5, 10);
     } catch (e) {
-        testCase.handleException(e);
+        await testCase.handleException(e);
     } finally {
-        testCase.saveResult();
+        await testCase.saveResult();
     }
 })().then(done);
+
+/*
+(async () => {
+    const testCase = new TestCase("Case 1");
+    const env = new Environment();
+    const mills = 200;
+    try {
+        await _navigateTo("https://www.lotto.de/lotto-6aus49"); // 1
+        await testCase.endOfStep("Open Lotto Page"); // 2
+        if (await _isVisible(_div("ppms_cm_popup_wrapper"))) {
+            await _focus(_button("ppms_cm_agree-to-all"));
+            await _highlight(_div("ppms_cm_popup_wrapper"));
+            await _highlight(_div("ppms_cm_centered_buttons"));
+            await _highlight(_button("ppms_cm_agree-to-all"));
+            await testCase.endOfStep("Popup-Handle");
+        } else {
+            await testCase.endOfStep("No popup found!");
+        }
+        await _wait(4000);
+
+
+        await _wait(3000);
+    } catch (e) {
+        await testCase.handleException(e);
+    } finally {
+        await testCase.saveResult();
+    }
+})().then(done);
+*/
--- packages/sakuli-commons/src/properties/decorator/properties.decorator.spec.ts
+++ packages/sakuli-commons/src/properties/decorator/properties.decorator.spec.ts
@@ -1,14 +1,15 @@
 import {getPropertyDecoratorDefinitions} from "./properties.decorator";
 import {DecoratedTestClass} from "../__mocks__";
 
+
 describe('Property decorator', () => {
 
 
     it('should get defined properties', () => {
         const jpDef = getPropertyDecoratorDefinitions(DecoratedTestClass);
         expect(jpDef.length).toBe(7);
-        expect(jpDef).toContainEqual({path: 'my.property.path', property: 'property'});
-        expect(jpDef).toContainEqual({path: 'property.2', property: 'property2'});
-        expect(jpDef).toContainEqual({path: 'property.alt', property: 'property2'})
+        expect(jpDef).toContainEqual({path: 'my.property.path', property: 'property', reader: expect.any(Function)});
+        expect(jpDef).toContainEqual({path: 'property.2', property: 'property2', reader: expect.any(Function)});
+        expect(jpDef).toContainEqual({path: 'property.alt', property: 'property2', reader: expect.any(Function)})
     })
 });
