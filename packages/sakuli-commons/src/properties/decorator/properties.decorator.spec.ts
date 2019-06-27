ceb0999c43348d20305bf48a1619581bce48d7ce
Tim Keiner <keiner.tim@gmail.com>
2019-06-27 16:30:51 +0200

    (#98) fixed test expectations

--- packages/sakuli-commons/src/properties/decorator/properties.decorator.spec.ts
+++ packages/sakuli-commons/src/properties/decorator/properties.decorator.spec.ts
@@ -6,7 +6,7 @@ describe('Property decorator', () => {
 
     it('should get defined properties', () => {
         const jpDef = getPropertyDecoratorDefinitions(DecoratedTestClass);
-        expect(jpDef.length).toBe(4);
+        expect(jpDef.length).toBe(7);
         expect(jpDef).toContainEqual({path: 'my.property.path', property: 'property'});
         expect(jpDef).toContainEqual({path: 'property.2', property: 'property2'});
         expect(jpDef).toContainEqual({path: 'property.alt', property: 'property2'})
