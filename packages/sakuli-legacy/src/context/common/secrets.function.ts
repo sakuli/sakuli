ded9b273d6321e09175272509a44a44c316269a1
Simon Hofmann <simon.hofmann@consol.de>
2019-06-28 17:15:22 +0200

    (#138) Encryption key is now read from property source

--- packages/sakuli-legacy/src/context/common/secrets.function.ts
+++ packages/sakuli-legacy/src/context/common/secrets.function.ts
@@ -1,30 +1,29 @@
 import {secret} from "@nut-tree/secrets";
 import {EncryptionError} from "./exceptions/encryptionerror.class";
+import {Project} from "@sakuli/core";
 
 export const MASTERKEY_ENV_KEY = "SAKULI_ENCRYPTION_KEY";
 export const MASTERKEY_CLI_KEY = "masterkey";
 export const MASTERKEY_PROPERTY_KEY = "sakuli.encryption.key";
 
-const getKeyFromEnvironment = () => {
-    const key = process.env[MASTERKEY_ENV_KEY];
+export const getEncryptionKey = (project: Project) => {
+    const key = project.get(MASTERKEY_ENV_KEY);
     if (!key) {
         throw new Error(`'${MASTERKEY_ENV_KEY}' is empty. Missing master key for secrets.`);
     }
     return key;
 };
 
-export async function decryptSecret(encryptedInput: string): Promise<string> {
+export async function decrypt(key: string, encryptedInput: string): Promise<string> {
     try {
-        const key = getKeyFromEnvironment();
         return secret.decrypt(encryptedInput, key);
     } catch (e) {
         throw new EncryptionError(e.message);
     }
 }
 
-export async function withEncryption<T>(encryptedInput: string, action: (input: string) => Promise<T>): Promise<T> {
+export async function withEncryption<T>(key: string, encryptedInput: string, action: (input: string) => Promise<T>): Promise<T> {
     try {
-        const key = getKeyFromEnvironment();
         const decrypted = await secret.decrypt(encryptedInput, key);
         return action(decrypted);
     } catch (e) {
