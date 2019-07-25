8cef83f65aea14650718c57aee704c33382f9f0c
Simon Hofmann <simon.hofmann@consol.de>
2019-07-25 23:31:31 +0200

    Added license check to plugin loading mechanism

--- packages/sakuli-cli/src/load-presets.function.ts
+++ packages/sakuli-cli/src/load-presets.function.ts
@@ -1,5 +1,16 @@
 import {SakuliPresetProvider} from "@sakuli/core";
 import {isPresent} from "@sakuli/commons";
+import {PluginValidator} from "@sakuli/plugin-validator";
+
+const LICENSE_KEY = 'SAKULI_LICENSE_KEY';
+
+const hasDefaultExport = (module: any) => {
+    return (module.default && typeof module.default === 'function');
+};
+
+const getUserToken = () => {
+    return process.env[LICENSE_KEY] || "";
+};
 
 export async function loadPresets(presetModuleNames: string[] = []): Promise<SakuliPresetProvider[]> {
     let potentialPresetModules: any[];
@@ -11,7 +22,14 @@ export async function loadPresets(presetModuleNames: string[] = []): Promise<Sak
         throw e;
     }
     return potentialPresetModules.map(([name, module]) => {
-        if (module.default && typeof module.default === 'function') {
+        if (hasDefaultExport(module)) {
+            const validator = new PluginValidator();
+            try {
+                validator.verifyPlugin(module, getUserToken());
+            } catch (e) {
+                console.warn(`Failed to enable plugin ${name}. Reason: ${e}`);
+                return null;
+            }
             return module.default as SakuliPresetProvider
         } else {
             console.warn(`${name} has no default export and is therefore ignored`);
