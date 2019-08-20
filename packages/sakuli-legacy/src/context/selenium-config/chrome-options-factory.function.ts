bdb9108931f777db9d7cdd83d7fddcf19ee5fac5
Tim Keiner <keiner.tim@gmail.com>
2019-08-20 14:03:06 +0200

    (#166) fixed windowsize property marshalling

--- packages/sakuli-legacy/src/context/selenium-config/chrome-options-factory.function.ts
+++ packages/sakuli-legacy/src/context/selenium-config/chrome-options-factory.function.ts
@@ -6,7 +6,7 @@ export const chromeOptionsFactory = (
     properties: ChromeProperties,
 ) => {
     const opts = new ChromeOptions();
-
+    console.log(properties);
     ifPresent(properties.arguments, prop => {
         opts.addArguments(...prop)
     });
@@ -16,8 +16,11 @@ export const chromeOptionsFactory = (
     });
 
     ifPresent(properties.windowSizeHeight, height => {
-        ifPresent(properties.windowSizeWith, width => {
-            opts.windowSize({height, width});
+        ifPresent(properties.windowSizeWidth, width => {
+            opts.windowSize({
+                height,
+                width
+            });
         })
     });
 
