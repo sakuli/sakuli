68ef8cbb7fe77b73cfd5b7a450d4d5a85a09fce0
Tim Keiner <keiner.tim@gmail.com>
2019-09-13 12:45:09 +0200

    (#218) add access method for uiOnly

--- packages/sakuli-legacy/src/loader/legacy-project-properties.class.spec.ts
+++ packages/sakuli-legacy/src/loader/legacy-project-properties.class.spec.ts
@@ -6,24 +6,53 @@ describe('LegacyProjectProperties', () => {
         props = new LegacyProjectProperties();
     });
 
-    it('should return browser value if set', () => {
-        props.browser = 'chrome';
-        expect(props.getBrowser()).toEqual('chrome');
+    describe('getBrowser', () => {
 
-    })
 
-    it('should return browser value if set', () => {
-        props.testsuiteBrowser = 'chrome';
-        expect(props.getBrowser()).toEqual('chrome');
-    })
+        it('should return browser value if set', () => {
+            props.browser = 'chrome';
+            expect(props.getBrowser()).toEqual('chrome');
 
-    it('should return browser value if set regardless of testsuiteBrowser value', () => {
-        props.browser = 'chrome';
-        props.testsuiteBrowser = 'ie';
-        expect(props.getBrowser()).toEqual('chrome');
-    })
+        })
 
-    it('should return firefox as default', () => {
-        expect(props.getBrowser()).toEqual('firefox');
-    })
+        it('should return browser value if set', () => {
+            props.testsuiteBrowser = 'chrome';
+            expect(props.getBrowser()).toEqual('chrome');
+        })
+
+        it('should return browser value if set regardless of testsuiteBrowser value', () => {
+            props.browser = 'chrome';
+            props.testsuiteBrowser = 'ie';
+            expect(props.getBrowser()).toEqual('chrome');
+        })
+
+        it('should return firefox as default', () => {
+            expect(props.getBrowser()).toEqual('firefox');
+        })
+
+    });
+
+    describe('isUiOnly', () => {
+
+
+        it('should return false by default', () => {
+            expect(props.isUiOnly()).toBe(false);
+        });
+
+        it('should return uiOnly when set', () => {
+            props.uiOnly = true;
+            expect(props.isUiOnly()).toBe(true);
+        })
+
+        it('should return uiOnly if set even testsuiteUiOnly is set', () => {
+            props.uiOnly = false;
+            props.testSuiteUiOnly = true;
+            expect(props.isUiOnly()).toBe(false);
+        })
+
+        it('should return testSuiteUiOnly', () => {
+            props.testSuiteUiOnly = true;
+            expect(props.isUiOnly()).toBe(true);
+        })
+    });
 });
--- packages/sakuli-legacy/src/loader/legacy-project-properties.class.ts
+++ packages/sakuli-legacy/src/loader/legacy-project-properties.class.ts
@@ -1,4 +1,4 @@
-import {BooleanProperty, NumberProperty, StringProperty, Maybe} from "@sakuli/commons";
+import {BooleanProperty, NumberProperty, StringProperty, Maybe, ifPresent} from "@sakuli/commons";
 import {Browsers} from "../context/selenium-config/create-driver-from-project.function";
 
 export class LegacyProjectProperties {
@@ -61,18 +61,56 @@ export class LegacyProjectProperties {
 
     /**
      * Will return the browser to use with Sakuli based on configuration. It will return value of
+     *
      * - [`browser`]{@link LegacyProjectProperties.browser} property if set
      * - otherwise [`testsuite.browser`]{@link LegacyProjectProperties.testsuiteBrowser} if set
      * - other `"firefox"`
+     *
      * @returns Browsers - a valid browser string that can be used by selenium
      */
     getBrowser(): Browsers {
         return this.browser || this.testsuiteBrowser || 'firefox'
     }
 
+    /**
+     * Determines if a testsuite should run in ui-only mode or not
+     * This option is usually defined via commandline and will override [testSuiteUiOnly]{@link LegacyProjectProperties.testSuiteUiOnly}
+
+     *
+     * To access the actual set browser within Sakuli use [isUiOnly()]{@link LegacyProjectProperties.isUiOnly}
+     *
+     * Possible values are defined in [Browsers]{@link Browsers} type
+     *
+     */
     @BooleanProperty('ui-only')
+    uiOnly: Maybe<boolean>
+
+    /**
+     * Defines the browser in witch the test suite should be executed
+     * This option is usually defined in `sakuli.properties` or `testsuite.properties` file and can be overridden by [uiOnly]{@link LegacyProjectProperties.uiOnly}
+     *
+     * To access the actual set browser within Sakuli use [isUiOnly()]{@link LegacyProjectProperties.isUiOnly}
+     *
+     */
     @BooleanProperty('testsuite.uiOnly')
-    uiOnly: boolean = false;
+    testSuiteUiOnly: Maybe<boolean>
+
+    /**
+     * Determines if a testsuite should run in ui-only mode or not
+     *
+     * - [`uiOnly`]{@link LegacyProjectProperties.uiOnly} property if set
+     * - otherwise [`testsuite.uiOnly`]{@link LegacyProjectProperties.testSuiteUiOnly} if set
+     * - other `false`
+     *
+     * @return boolean
+     */
+    isUiOnly(): boolean {
+        return ifPresent(this.uiOnly,
+            uiOnly => uiOnly,
+            () => ifPresent(this.testSuiteUiOnly,
+                tsUiOnly => tsUiOnly,
+                () => false))
+    }
 
     /**
      */
