b1e8a05db82fd65b4889384e1b77b1c9bf9e6117
Tim Keiner <keiner.tim@gmail.com>
2019-10-09 11:13:49 +0200

    (#241) add docs for actino api

--- packages/sakuli-legacy/src/context/sahi/action/mouse-action/mouse-action-api.interface.ts
+++ packages/sakuli-legacy/src/context/sahi/action/mouse-action/mouse-action-api.interface.ts
@@ -41,7 +41,8 @@ export interface MouseActionApi {
 
     /**
      *
-     * Performs a `release` action on the element
+     * Performs a `release` action on the element.
+     * Release is the action when a user releases his finger from the mouse button.
      *
      * @see https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/input_exports_Actions.html
      *
@@ -52,18 +53,88 @@ export interface MouseActionApi {
      */
     _mouseUp(query: SahiElementQueryOrWebElement, isRight?: boolean, combo?: string): Promise<void>
 
+    /**
+     * Performs a right click on the queried element. Can be combined with a key combo.
+     *
+     * @param query
+     * @param combo - Any combo key: can be "CTRL", "SHIFT", "ALT" or "META";
+     Can also be two or more keys together like "CTRL|SHIFT"
+     */
     _rightClick(query: SahiElementQueryOrWebElement, combo?: string): Promise<void>
 
+    /**
+     * Invokes a mouse over on the queried element. Can be combined with a key combo.
+     *
+     * @param query
+     * @param combo
+     */
     _mouseOver(query: SahiElementQueryOrWebElement, combo?: string): Promise<void>
 
+    /**
+     * Performs a click on a checkbox input and forces its state to be checked
+     *
+     * @param query
+     */
     _check(query: SahiElementQueryOrWebElement): Promise<void>
 
+    /**
+     * Performs a click on a checkbox input and forces its state not to be checked
+     *
+     * @param query
+     */
     _uncheck(query: SahiElementQueryOrWebElement): Promise<void>
 
+    /**
+     * Performs a mouse down on _eSource_, than moves the mouse to the position of element _eTarget_ and releases the mouse button at this position.
+     *
+     * This implementation uses Webdriver actions which maybe not always behave as expected.
+     *
+     * For a more reliable experience {@link ThenableRegion.dragAndDrop} is recommended.
+     *
+     * @param eSource
+     * @param eTarget
+     */
     _dragDrop(eSource: SahiElementQueryOrWebElement, eTarget: SahiElementQueryOrWebElement): Promise<void>
 
+    /**
+     * Performs a mouse down on _eSource_, than moves the mouse to the position P(x,y) in the browsers viewport and releases the mouse button at this position.
+     *
+     * This implementation uses Webdriver actions which maybe not always behave as expected.
+     *
+     * For a more reliable experience {@link ThenableRegion.dragAndDrop} is recommended.
+     *
+     * @param q
+     * @param x
+     * @param y
+     * @param $isRelative
+     */
     _dragDropXY(q: SahiElementQueryOrWebElement, x: number, y: number, $isRelative?: boolean): Promise<void>
 
+    /**
+     * Sets the options within a `<select>` element.
+     * Selection can be done by the acutal values of an option or it's zero-based element index.
+     *
+     * This action will invoke a click on the option element which means, that the _selected_ state is not enforced. If an option is already selected it will be unselected in this case.
+     *
+     * @example
+     * Assume the following HTML-Snippet:
+     * ```html
+     * <select multiple="multiple" name="cities">
+     *     <option value="muc">Munich</option>
+     *     <option value="vie">Vienna</option>
+     *     <option value="dus">Dusseldorf</option>
+     * </select>
+     * ```
+     *
+     * ```typescript
+     * await _setSelected(_select('cities'), 'vie') // -> select the option Vienna
+     * await _setSelected(_select('cities'), 0) // -> select the option Dusseldorf
+     * ```
+     *
+     * @param query
+     * @param optionToSelect
+     * @param isMultiple
+     */
     _setSelected(query: SahiElementQueryOrWebElement, optionToSelect: string | number | string[] | number[], isMultiple?: boolean): Promise<void>
 
 }
