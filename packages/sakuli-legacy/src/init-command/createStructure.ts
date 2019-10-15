8da6fde61f3819dd9f300304b19f82e9760b6411
Cliff <cliff.le.van@consol.de>
2019-10-15 15:14:35 +0200

    (#22) changed the way the files are written

--- packages/sakuli-legacy/src/init-command/createStructure.ts
+++ packages/sakuli-legacy/src/init-command/createStructure.ts
@@ -1,30 +1,11 @@
-import {openSync, appendFileSync, mkdirSync, writeSync, readFileSync} from "fs";
+import { appendFileSync, mkdirSync } from "fs";
 
-export const createStructureAndFillConfig = (directory: string, suitName: string, startUrl: string) => {
-    const newDir = directory + '/' + suitName;
-    createStructure(newDir);
-    fillProperties(newDir + '/testsuite.properties',
-        readAndReplaceConfigFile('src/init-command/config/testsuite.properties', newDir));
+export const createStructureAndFillConfig = (directory: string, suiteName: string, startUrl: string) => {
+    const rootDir = directory + `/${suiteName}`;
+    mkdirSync(rootDir, {recursive: true});
+    appendFileSync(rootDir + '/testsuite.properties', `testsuite.id=${suiteName}\ntestsuite.browser=chrome`);
+    appendFileSync(rootDir + '/testsuite.suite', `case1/check.js ${startUrl}`);
 
-    fillProperties(newDir + '/testsuite.suite', 'case1/check.js ' + startUrl);
-};
-
-const fillProperties = (filePath: string, input: string) => {
-    const file = openSync(filePath, 'w');
-    writeSync(file, new Buffer(input))
-};
-
-const createStructure = (directory: string) => {
-    mkdirSync(directory, {recursive: true});
-    appendFileSync(directory + '/testsuite.properties', "");
-    appendFileSync(directory + '/testsuite.suite', "");
-
-    mkdirSync(directory + '/case1', {recursive: true});
-    appendFileSync(directory + '/case1/check.js', "");
-};
-
-const readAndReplaceConfigFile = (dir: string, prop: string) => {
-    const content = readFileSync(dir, 'utf8');
-    return content.replace(/{{suiteName}}/g, prop);
-
+    mkdirSync(rootDir + '/case1', {recursive: true});
+    appendFileSync(rootDir + '/case1/check.js', "");
 };
