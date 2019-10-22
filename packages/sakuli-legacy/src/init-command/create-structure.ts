bf0876c85c2c2a5b9a4209cffa826b2d3d93d5e7
Cliff Le Van <cliff.le.van@consol.de>
2019-10-22 13:21:10 +0200

    (#22) refactored createTestsuite

--- packages/sakuli-legacy/src/init-command/create-structure.ts
+++ packages/sakuli-legacy/src/init-command/create-structure.ts
@@ -1,19 +1,29 @@
-import { appendFileSync, mkdirSync } from "fs";
-import {stripIndents} from 'common-tags';
+import { mkdirSync, writeFileSync } from "fs";
+import { stripIndent } from 'common-tags';
 
 export const createTestsuite = (directory: string, suiteName: string) => {
     const rootDir = directory + `/${suiteName}`;
     mkdirSync(rootDir, {recursive: true});
-    appendFileSync(
-        rootDir + '/testsuite.properties',
-        stripIndents`testsuite.id=${suiteName}
-        testsuite.browser=chrome`
-    );
-    appendFileSync(
-        rootDir + '/testsuite.suite',
-        'case1/check.js https://sakuli.io'
-    );
+    createTestsuiteProperties(rootDir, suiteName);
+    createTestsuiteSuite(rootDir);
+    createTestcaseInTestsuite(rootDir);
+};
 
-    mkdirSync(rootDir + '/case1', {recursive: true});
-    appendFileSync(rootDir + '/case1/check.js', "");
+const createTestsuiteProperties = (path: string, suiteName: string) => {
+    writeFileSync(
+        `${path}/testsuite.properties`,
+        stripIndent`
+        testsuite.id=${suiteName}
+        testsuite.browser=chrome
+        `,
+        {encoding: 'utf8'});
+};
+
+const createTestsuiteSuite = (path: string) => {
+    writeFileSync(`${path}/testsuite.suite`, 'case1/check.js https://sakuli.io', {encoding: 'utf8'});
+};
+
+const createTestcaseInTestsuite = (path: string) => {
+    mkdirSync(`${path}/case1`, {recursive: true});
+    writeFileSync(`${path}/case1/check.js`, "", {encoding: 'utf8'});
 };
