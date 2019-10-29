8bc3656bc04bdfbc2421d4b1ba4a391b04c465cc
Cliff Le Van <cliff.le.van@consol.de>
2019-10-29 10:11:02 +0100

    (#22) externalized generated files to module

--- packages/sakuli-legacy/src/init-command/create-structure.ts
+++ packages/sakuli-legacy/src/init-command/create-structure.ts
@@ -1,34 +1,12 @@
 import { mkdirSync, writeFileSync } from "fs";
-import { stripIndent } from 'common-tags';
+import { packageJson, testsuiteProperties } from "./create-structure-files";
 
 export const createTestsuiteDirectory = (directory: string) => {
     mkdirSync(directory, {recursive: true});
 };
 
 export const createTestsuiteProperties = (path: string, suiteName: string) => {
-    writeFileSync(
-        `${path}/testsuite.properties`,
-        stripIndent`
-        testsuite.id=${suiteName}
-        testsuite.browser=chrome
-        #testsuite.name=\${testsuite.id}
-        #testsuite.warningTime=0
-        #testsuite.criticalTime=0
-        #testsuite.uiOnly=false
-        
-        #log.level=INFO
-        #sakuli.log.maxAge=14
-        #sakuli.log.folder=\${sakuli.testsuite.folder}/_logs
-        #sakuli.environment.similarity.default=0.99
-        #sakuli.autoHighlight=false
-        #sakuli.highlight.seconds=0.2
-        #sakuli.typeDelay=300
-        #sakuli.clickDelay=0.2
-        #sakuli.encryption.key=""
-        #sakuli.screenshot.onError=true
-        #sakuli.screenshot.dir=\${sakuli.log.folder}/_screenshots
-        `,
-        {encoding: 'utf8'});
+    writeFileSync(`${path}/testsuite.properties`, testsuiteProperties(suiteName),{encoding: 'utf8'});
 };
 
 export const createTestsuiteSuite = (path: string) => {
@@ -44,20 +22,5 @@ export const createTestcaseFile = (path: string) => {
 };
 
 export const createPackageJson = (path: string, suiteName: string) => {
-    const packageJson = {
-        name: suiteName,
-        version: "1.0.0",
-        description: "",
-        main: "index.js",
-        scripts: {
-            test: `sakuli run ${suiteName}`
-        },
-        author: "",
-        license: "ISC",
-        dependencies: {
-            "@sakuli/cli": "^2.1.3",
-        },
-    };
-
-    writeFileSync(`${path}/package.json`, JSON.stringify(packageJson, null, 4));
+    writeFileSync(`${path}/package.json`, packageJson(suiteName));
 };
