ce40a1ab016421c96a4190167de37fbaae261614
Tim Keiner <keiner.tim@gmail.com>
2019-11-18 11:29:52 +0100

    (#242) add test for feature config bootstrapping

---
+++ packages/sakuli-cli/src/commands/enable-enterprise/tasks/configure-feature-task.function.spec.ts
@@ -0,0 +1,48 @@
+import { configureFeatureTask } from "./configure-feature-task.function"
+import { promises as fs } from 'fs';
+import { join, sep } from "path";
+import { tmpdir } from "os";
+
+describe('configureFeatureTask', () => {
+
+    let fsRoot: string;
+    beforeEach(async () => {
+        fsRoot = await fs.mkdtemp(`${tmpdir()}${sep}`);
+    });
+
+    afterEach(async () => {
+        try {
+            await fs.rmdir(fsRoot)
+        } catch (e) {
+        }
+    })
+
+    it('should write configuration to sakuli.properties', async () => {
+        // GIVEN
+        const task = configureFeatureTask({
+            "KEY": "value"
+        }, fsRoot);
+
+        // WHEN
+        await task();
+        const propContent = await (fs.readFile(join(fsRoot, 'sakuli.properties')).then(buf => buf.toString()));
+        // THEN
+        expect(propContent).toContain("KEY=value");
+    })
+
+    it('should append configuration when file has contents', async () => {
+        // GIVEN
+        await fs.writeFile(join(fsRoot, 'sakuli.properties'), "EXISTING=value\n")
+        const task = configureFeatureTask({
+            "KEY": "value"
+        }, fsRoot);
+
+        // WHEN
+        await task();
+        const propContent = await (fs.readFile(join(fsRoot, 'sakuli.properties')).then(buf => buf.toString()));
+        // THEN
+        expect(propContent).toContain("EXISTING=value");
+        expect(propContent).toContain("KEY=value");
+    })
+
+})
--- packages/sakuli-cli/src/commands/enable-enterprise/tasks/configure-feature-task.function.ts
+++ packages/sakuli-cli/src/commands/enable-enterprise/tasks/configure-feature-task.function.ts
@@ -3,13 +3,26 @@ import { join } from "path";
 
 export const recordToPropertiesString = (record: Record<string, string>) => Object
     .entries(record)
-    .map(([key, value]) => `${key=value}`)
+    .map(([key, value]) => `${key}=${value}`)
     .join('\n')
 
-export const configureFeatureTask = (configurationItems: Record<string, string>) => async () => {
+/**
+ * Creates a task that will convert the configurationRecords (key - value pairs)
+ * into proper property-file format string and writes it into sakuli.properties
+ *
+ * We assume that the configuration for all enterprise features can be bootstrapped
+ * in the sakuli.properties which means they apply for all testsuites
+ *
+ * @param configurationRecord - key - value pairs which are written to properties file
+ * @param path - the path to `sakuli.properties`
+ */
+export const configureFeatureTask = (
+    configurationRecord: Record<string, string>,
+    path: string = process.cwd()
+) => async () => {
     await fs.writeFile(
-        join(process.cwd(), 'sakuli.properties'),
-        recordToPropertiesString(configurationItems),
+        join(path, 'sakuli.properties'),
+        recordToPropertiesString(configurationRecord),
         { flag: 'a' }
     );
 }
