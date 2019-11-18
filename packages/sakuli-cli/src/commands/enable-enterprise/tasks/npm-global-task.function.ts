8aa39165bf961177787fc229ef53f177d8ece3d7
Tim Keiner <keiner.tim@gmail.com>
2019-11-18 14:55:35 +0100

    (#242) add tests for npm global task

--- packages/sakuli-cli/src/commands/enable-enterprise/tasks/npm-global-task.function.ts
+++ packages/sakuli-cli/src/commands/enable-enterprise/tasks/npm-global-task.function.ts
@@ -5,7 +5,7 @@ import { promises as fs } from 'fs'
 const npmRcContent = (key: string) => `//registry.npmjs.org/:_authToken=${key}`;
 
 export const npmGlobalTask = (key: string) => async () => {
-    const npmRcFile = join(homedir(), '._npmrc');
+    const npmRcFile = join(homedir(), '.npmrc');
     await fs.writeFile(npmRcFile, npmRcContent(key), {flag: 'a'});
 
 }
