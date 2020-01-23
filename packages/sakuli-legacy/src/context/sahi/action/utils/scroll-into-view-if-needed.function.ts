141eb21afab34e62cdca50715e9c70ad8903288d
Sven Hettwer <sven.hettwer@consol.de>
2020-01-23 09:48:16 +0100

    (#314) use stringified object for logging

--- packages/sakuli-legacy/src/context/sahi/action/utils/scroll-into-view-if-needed.function.ts
+++ packages/sakuli-legacy/src/context/sahi/action/utils/scroll-into-view-if-needed.function.ts
@@ -6,7 +6,7 @@ export async function scrollIntoViewIfNeeded(element: WebElement, ctx: TestExecu
         ctx.logger.debug("scroll into view failed: element was null or undefined");
         return Promise.resolve();
     }
-    ctx.logger.trace(`scroll into view started with element: ${element}`);
+    ctx.logger.trace(`scroll into view started with element: ${JSON.stringify(element)}`);
     return element.getDriver().executeScript<void>(`
         return arguments[0].scrollIntoView({
             inline: "center",
