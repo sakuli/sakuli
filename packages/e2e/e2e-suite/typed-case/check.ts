81e4158e72db29d9bffe13d453f74e795642b2d3
Simon Hofmann <simon.hofmann@consol.de>
2020-02-03 13:52:47 +0100

    (#266) Removed monitoring thresholds for e2e tests on ci

--- packages/e2e/e2e-suite/typed-case/check.ts
+++ packages/e2e/e2e-suite/typed-case/check.ts
@@ -2,9 +2,9 @@
     const testCase: TestCase = new TestCase("My Typescript based test");
     try {
         await _navigateTo("https://sakuli.io");                                  // 1
-        testCase.endOfStep("TS Open Landing Page",5);             // 2
+        testCase.endOfStep("TS Open Landing Page");             // 2
         await _click(_link("Getting started"));                         // 3
-        testCase.endOfStep("TS Navigate to Getting Started",3);   // 4
+        testCase.endOfStep("TS Navigate to Getting Started");   // 4
         await _highlight(_code("npm init"));                            // 5
         testCase.endOfStep("Find npm init code sample");
     } catch (e) {
