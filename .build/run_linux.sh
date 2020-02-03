aedf081343c3812cb8e7d76c2bdcb14e50aa9b36
Simon Hofmann <simon.hofmann@consol.de>
2020-02-03 11:38:15 +0100

    (#266) Enabled e2e tests for linux

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -6,4 +6,5 @@ echo $PWD
 npm ci
 lerna run build > /dev/null 2>&1
 FIREFOX_WD_URL=http://localhost:4444/wd/hub CHROME_WD_URL=http://localhost:4445/wd/hub npm test -- --runInBand --ci --bail
+lerna run test:e2e
 lerna run typedoc
