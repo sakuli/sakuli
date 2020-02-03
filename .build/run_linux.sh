7056e2d889871bcbf7dd74486d0e4aa65041d5ee
Simon Hofmann <simon.hofmann@consol.de>
2020-02-03 11:19:50 +0100

    (#266) Removed travis_wait from scripts

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -3,7 +3,7 @@ set -e
 
 echo $PWD
 
-travis_wait npm ci
+npm ci
 lerna run build > /dev/null 2>&1
 FIREFOX_WD_URL=http://localhost:4444/wd/hub CHROME_WD_URL=http://localhost:4445/wd/hub npm test -- --runInBand --ci --bail
 lerna run typedoc
--- .build/run_osx.sh
+++ .build/run_osx.sh
@@ -3,7 +3,7 @@ set -e
 
 echo $PWD
 
-travis_wait npm ci
+npm ci
 lerna run build > /dev/null 2>&1
 lerna run test:e2e
 
