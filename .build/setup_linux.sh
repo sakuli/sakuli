6136d37bffc94a0238b298240436b3fd7a1e5e3f
Simon Hofmann <simon.hofmann@consol.de>
2020-02-03 11:30:30 +0100

    (#266) Removed docker pull for nut-ci image

--- .build/setup_linux.sh
+++ .build/setup_linux.sh
@@ -9,7 +9,6 @@ sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 sudo apt-get install -y build-essential libxt-dev libxtst-dev libpng++-dev libxinerama-dev
 docker pull selenium/standalone-chrome-debug
 docker pull selenium/standalone-firefox-debug
-docker pull s1hofmann/nut-ci
 docker run -d -p 4444:4444 selenium/standalone-firefox-debug
 docker run -d -p 4445:4444 selenium/standalone-chrome-debug
 npm i -g lerna gh-pages
