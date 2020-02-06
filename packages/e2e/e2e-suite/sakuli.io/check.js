8cb468dfe0e29185650c29d67c0ff6d5ec114d85
Simon Hofmann <simon.hofmann@consol.de>
2020-02-06 09:46:04 +0100

    (#266) Updated testcase according to review findings

--- packages/e2e/e2e-suite/sakuli.io/check.js
+++ packages/e2e/e2e-suite/sakuli.io/check.js
@@ -4,12 +4,19 @@
     try {
         await _navigateTo(url + "/enterprise");
         await testCase.endOfStep("Opened Enterprise page.");
+
+        const cookieBannerButton = _button("I agree");
+        if (await _isVisible(cookieBannerButton)) {
+            await _highlight(cookieBannerButton);
+            await _click(cookieBannerButton);
+        }
+        await testCase.endOfStep("Close cookie banner");
+
         const links = await _collect("_link", /Contact/);
-        await _focus(links[1]);
         await _highlight(links[1]);
         await _click(links[1]);
         await testCase.endOfStep("Contact form clicked.");
-        await _wait(3000);
+        await _assert(_isVisible(_heading1(/Contact our Team/)));
     } catch (e) {
         await testCase.handleException(e);
     } finally {
