2c06198bc73a3a9d21f35eff04644d8256585acf
Simon Hofmann <simon.hofmann@consol.de>
2020-02-07 12:13:20 +0100

    (#266) Moved sonar-scanner run to run_linux.sh

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -6,5 +6,8 @@ echo $PWD
 npm ci
 lerna run build > /dev/null 2>&1
 FIREFOX_WD_URL=http://localhost:4444/wd/hub CHROME_WD_URL=http://localhost:4445/wd/hub npm test -- --runInBand --ci --bail
+if [[ $TRAVIS_NODE_VERSION = "lts/erbium" ]]; then
+  sonar-scanner
+fi
 npm run test:e2e
 lerna run typedoc
