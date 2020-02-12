e14e56a2205297b18ccd2679d461554a219642c0
Simon Hofmann <simon.hofmann@consol.de>
2020-02-12 14:11:01 +0100

    Added apt-get updated step to update package index

--- .build/setup_linux.sh
+++ .build/setup_linux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+sudo apt-get update
 sudo apt-get install -y build-essential libxt-dev libxtst-dev libpng++-dev libxinerama-dev
 docker pull selenium/standalone-chrome-debug
 docker pull selenium/standalone-firefox-debug
