15fa50b1a17ee8bedafef3191fa4703fe6b3d945
Simon Hofmann <simon.hofmann@consol.de>
2020-02-12 14:07:59 +0100

    Removed explicit Docker install

--- .build/setup_linux.sh
+++ .build/setup_linux.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-
 sudo apt-get install -y build-essential libxt-dev libxtst-dev libpng++-dev libxinerama-dev
 docker pull selenium/standalone-chrome-debug
 docker pull selenium/standalone-firefox-debug
