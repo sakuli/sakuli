48a835fac310eb2ba92d9005525e706bb31479cf
Simon Hofmann <simon.hofmann@consol.de>
2020-02-21 08:21:09 +0100

    (#355) Removed duplicated test

--- packages/sakuli-legacy/src/context/sahi/action/common-action/common-actions.spec.ts
+++ packages/sakuli-legacy/src/context/sahi/action/common-action/common-actions.spec.ts
@@ -137,29 +137,6 @@ describe('common-actions', () => {
             expect(result).toBeTruthy();
             expect(duration).toBeLessThan(2_000);
         });
-        it('should return true if update interval < default check interval', async () => {
-            // GIVEN
-            await driver.get(mockHtml(`
-                <div id="root">
-                </div>
-                <script>
-                const elem = document.getElementById("root");
-                const interval = setInterval(() => {
-                    elem.innerHTML = Date.now();
-                }, 100);
-                setTimeout(() => clearInterval(interval), 1000);
-                </script>
-            `));
-
-            // WHEN
-            const start = Date.now();
-            const result = await api._pageIsStable();
-            const duration = Date.now() - start;
-
-            // THEN
-            expect(result).toBeTruthy();
-            expect(duration).toBeLessThan(2_000);
-        });
         it('should return false for unstable DOM', async () => {
             // GIVEN
             await driver.get(mockHtml(`
