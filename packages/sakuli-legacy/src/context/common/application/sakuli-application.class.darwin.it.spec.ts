763db4de0d067da2f5f842593ce84f90c16db54d
Simon Hofmann <simon.hofmann@consol.de>
2020-02-28 10:22:46 +0100

    (#309) Reverted SUT on macOS

--- packages/sakuli-legacy/src/context/common/application/sakuli-application.class.darwin.it.spec.ts
+++ packages/sakuli-legacy/src/context/common/application/sakuli-application.class.darwin.it.spec.ts
@@ -5,7 +5,7 @@ import {Application} from "./application.interface";
 import {prepareContext} from "../actions/__mocks__/prepare-context.function";
 
 describe("Application", () => {
-    const application = "/Applications/Insomnia.app/Contents/MacOS/Insomnia";
+    const application = "/Applications/Calculator.app/Contents/MacOS/Calculator";
     const mockProject = new Project(".");
     let ctx: TestExecutionContext;
 
