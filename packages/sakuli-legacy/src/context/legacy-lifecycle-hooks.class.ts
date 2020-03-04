a2cb27fa36d117f80306458daba12edf7c07f6ea
Simon Hofmann <simon.hofmann@consol.de>
2020-03-04 16:27:47 +0100

    (#371) Pass testsuite warning and critical time when starting a new execution

--- packages/sakuli-legacy/src/context/legacy-lifecycle-hooks.class.ts
+++ packages/sakuli-legacy/src/context/legacy-lifecycle-hooks.class.ts
@@ -1,23 +1,22 @@
-import { Builder, ThenableWebDriver } from 'selenium-webdriver';
-import { ifPresent, Maybe, throwIfAbsent } from "@sakuli/commons";
-import { createTestCaseClass } from "./common/test-case";
-import { Key } from "./common/key.class";
-import { sahiApi } from "./sahi/api";
-import { Project, TestExecutionContext, TestExecutionLifecycleHooks } from "@sakuli/core";
-import { TestFile } from "@sakuli/core/dist/loader/model/test-file.interface";
-import { basename, dirname, join, parse, sep } from "path";
-import { createLoggerObject } from "./common/logger";
-import { LegacyProjectProperties } from "../loader/legacy-project-properties.class";
-import { promises as fs } from "fs";
-import { MouseButton } from "./common/button.class";
-import { createThenableApplicationClass } from "./common/application";
-import { createThenableEnvironmentClass } from "./common/environment";
-import { createThenableRegionClass } from "./common/region";
-import { LegacyApi } from "./legacy-api.interface";
-import { createDriverFromProject } from "./selenium-config/create-driver-from-project.function";
-import { TestStepCache } from './common/test-case/steps-cache/test-step-cache.class';
-import { NoopSahiApi } from './noop-sahi-api.const'
-import { SahiApi } from './sahi/sahi-api.interface';
+import {Builder, ThenableWebDriver} from 'selenium-webdriver';
+import {ifPresent, Maybe, throwIfAbsent} from "@sakuli/commons";
+import {createTestCaseClass} from "./common/test-case";
+import {Key, MouseButton} from "./common";
+import {sahiApi} from "./sahi/api";
+import {Project, TestExecutionContext, TestExecutionLifecycleHooks} from "@sakuli/core";
+import {TestFile} from "@sakuli/core/dist/loader/model/test-file.interface";
+import {basename, dirname, join, parse, sep} from "path";
+import {createLoggerObject} from "./common/logger";
+import {LegacyProjectProperties} from "../loader/legacy-project-properties.class";
+import {promises as fs} from "fs";
+import {createThenableApplicationClass} from "./common/application";
+import {createThenableEnvironmentClass} from "./common/environment";
+import {createThenableRegionClass} from "./common/region";
+import {LegacyApi} from "./legacy-api.interface";
+import {createDriverFromProject} from "./selenium-config/create-driver-from-project.function";
+import {TestStepCache} from './common/test-case/steps-cache/test-step-cache.class';
+import {NoopSahiApi} from './noop-sahi-api.const'
+import {SahiApi} from './sahi/sahi-api.interface';
 
 export class LegacyLifecycleHooks implements TestExecutionLifecycleHooks {
 
@@ -49,7 +48,9 @@ export class LegacyLifecycleHooks implements TestExecutionLifecycleHooks {
         const id = properties.testsuiteId
             ? properties.testsuiteId
             : project.rootDir.split(sep).pop();
-        testExecutionContext.startTestSuite({id})
+        const warningTime = properties.testsuiteWarningTime || 0;
+        const criticalTime = properties.testsuiteCriticalTime || 0;
+        testExecutionContext.startTestSuite({id, warningTime, criticalTime})
     }
 
     async afterExecution(project: Project, testExecutionContext: TestExecutionContext) {
@@ -86,7 +87,7 @@ export class LegacyLifecycleHooks implements TestExecutionLifecycleHooks {
 
     async requestContext(ctx: TestExecutionContext, project: Project): Promise<LegacyApi> {
         const sahi: SahiApi = this.driver ? sahiApi(this.driver, ctx) : NoopSahiApi;
-        const currentTestFolder = throwIfAbsent(this.currentTest, Error('Could not initialize LegacyDslContext because no test folder was found / provided'))
+        const currentTestFolder = throwIfAbsent(this.currentTest, Error('Could not initialize LegacyDslContext because no test folder was found / provided'));
         const stepCacheFileName = basename(this.currentFile);
         const stepsCache = new TestStepCache(join(currentTestFolder, `.${stepCacheFileName}.steps.cache`));
         return Promise.resolve({
@@ -102,7 +103,5 @@ export class LegacyLifecycleHooks implements TestExecutionLifecycleHooks {
             $includeFolder: '',
             ...sahi,
         });
-
     }
-
 }
