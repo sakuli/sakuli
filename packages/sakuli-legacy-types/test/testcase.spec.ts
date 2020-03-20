b76d9fabf4c3f4c5793cbff3d1c2b34bfb50c75d
Van Cliff Le <cliff.le.van@consol.de>
2020-03-20 13:40:03 +0100

    (#386) replaced type for NewableThenableApplication

--- packages/sakuli-legacy-types/test/testcase.spec.ts
+++ packages/sakuli-legacy-types/test/testcase.spec.ts
@@ -6,15 +6,17 @@
 (async () => {
     const testCase: TestCase = new TestCase("My Typescript based test");
     const screen = new Region();
+    const chromium = new Application("chromium-browser");
     try {
         await _navigateTo("https://sakuli.io");                                  // 1
         testCase.endOfStep("TS Open Landing Page",5);             // 2
         await _click(_link("Getting started"));                         // 3
         await _highlight(_code("npm init"));                            // 5
         await screen.find("foo.png");
+        await chromium.open();
     } catch (e) {
         await testCase.handleException(e);
     } finally {
         await testCase.saveResult();
     }
-})().then(done);
+})();
