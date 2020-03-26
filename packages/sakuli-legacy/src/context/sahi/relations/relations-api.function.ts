a5880cbfe78480efe136206df77d2ae1b6a602ff
Van Cliff Le <cliff.le.van@consol.de>
2020-03-26 15:22:41 +0100

    (#395) fixed return condition for _underOrAbove

--- packages/sakuli-legacy/src/context/sahi/relations/relations-api.function.ts
+++ packages/sakuli-legacy/src/context/sahi/relations/relations-api.function.ts
@@ -1,17 +1,17 @@
-import {By, ILocation, ISize, ThenableWebDriver, WebElement} from "selenium-webdriver";
-import {TestExecutionContext} from "@sakuli/core";
-import {RelationProducer, RelationProducerWithOffset, SahiRelation} from "./sahi-relation.interface";
-import {ifPresent, mapAsync} from "@sakuli/commons";
-import {edges} from "./edges.function";
-import {isLeftOf, isRightOf} from "./vector2.type";
-import {AccessorUtil} from "../accessor";
-import {SahiElementQuery, SahiElementQueryOrWebElement} from "../sahi-element.interface";
-import {isChildOf} from "../helper/is-child-of.function";
-import {distanceBetween} from "../helper/distance-between.function";
-import {getSiblingIndex} from "../helper/get-sibling-index.function";
-import {getParent} from "../helper/get-parent.function";
-import {parentApi} from "./parent-api.function";
-import {PositionalInfo, positionalInfo} from "./positional-info.function";
+import { By, ThenableWebDriver, WebElement } from "selenium-webdriver";
+import { TestExecutionContext } from "@sakuli/core";
+import { RelationProducer, RelationProducerWithOffset, SahiRelation } from "./sahi-relation.interface";
+import { ifPresent, mapAsync } from "@sakuli/commons";
+import { edges } from "./edges.function";
+import { isLeftOf, isRightOf } from "./vector2.type";
+import { AccessorUtil } from "../accessor";
+import { SahiElementQuery, SahiElementQueryOrWebElement } from "../sahi-element.interface";
+import { isChildOf } from "../helper/is-child-of.function";
+import { distanceBetween } from "../helper/distance-between.function";
+import { getSiblingIndex } from "../helper/get-sibling-index.function";
+import { getParent } from "../helper/get-parent.function";
+import { parentApi } from "./parent-api.function";
+import { PositionalInfo, positionalInfo } from "./positional-info.function";
 import { RelationApi } from "./relations-api.interface";
 
 export function relationsApi(
@@ -132,7 +132,8 @@ export function relationsApi(
         return createVerticalRelation(query, offset, (a, b) => {
             const edgesA = edges(a);
             const edgesB = edges(b);
-            return (edgesB.isAbove(edgesA) || edgesB.isUnder(edgesA)) && edgesB.intersectsVertical(edgesA);
+            return (edgesB.isAbove(edgesA) || edgesB.isUnder(edgesA)) &&
+                (edgesB.intersectsVertical(edgesA) || edgesA.intersectsVertical(edgesB));
         })
     }
 
