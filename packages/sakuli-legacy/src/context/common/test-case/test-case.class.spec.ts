d5222ba2c191cf2688f637e15d33ff58d065d8e1
Van Cliff Le <cliff.le.van@consol.de>
2020-04-14 12:27:11 +0200

    (#340) added additional test for flat storage

--- packages/sakuli-legacy/src/context/common/test-case/test-case.class.spec.ts
+++ packages/sakuli-legacy/src/context/common/test-case/test-case.class.spec.ts
@@ -279,6 +279,26 @@ describe("TestCase", () => {
             // THEN
             expect(ScreenApi.takeScreenshotWithTimestamp).toBeCalledWith(`${tmpdir()}/UNKNOWN_TESTSUITE_testcase_1/error_UNKNOWN_TESTSUITE_testcase_1`);
         });
+
+        it("should take a screenshot and save it flat when explicitly specified in props", async () => {
+            // GIVEN
+            const testFolder = "testCaseFolder";
+            const legacyProps = new LegacyProjectProperties();
+            legacyProps.screenshotDir = tmpdir();
+            legacyProps.screenshotStorage = "flat";
+            project = mockPartial<Project>({
+                objectFactory: jest.fn().mockReturnValue(legacyProps)
+            });
+            const SUT = createTestCaseClass(testExecutionContext, project, testFolder);
+            const tc = new SUT("testId", 0, 0);
+            const testError = new Error("testError");
+
+            // WHEN
+            await tc.handleException(testError);
+
+            // THEN
+            expect(ScreenApi.takeScreenshotWithTimestamp).toBeCalledWith(`${tmpdir()}/UNKNOWN_TESTSUITE_testcase_1/error_UNKNOWN_TESTSUITE_testcase_1`);
+        });
     });
 
     describe('caching', () => {
