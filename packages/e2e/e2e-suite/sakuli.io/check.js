81b80cedeba025b709b226276dd07aa8ac0cd355
Van Cliff Le <cliff.le.van@consol.de>
2020-04-15 11:57:07 +0200

    (#340) changed starting page of testcases

--- packages/e2e/e2e-suite/sakuli.io/check.js
+++ packages/e2e/e2e-suite/sakuli.io/check.js
@@ -1,18 +1,16 @@
 (async () => {
     const testCase = new TestCase("My Typescript based test");
     const env = new Environment();
-    
+
     async function getControlKey() {
         return await env.isDarwin() ? Key.CMD : Key.CTRL;
     }
     try {
-        await _navigateTo("https://sakuli.io");
-        testCase.endOfStep("TS Open Landing Page");
-        await _click(_link("EXPLORE"));
-        testCase.endOfStep("TS Open Explore page");
-        await _pageIsStable();
+        await _navigateTo("https://sakuli.io/contact/");
+        await _assertExists(_heading2("Get In Touch"));
+        testCase.endOfStep("Open Contact Page");
         await _click(_link("Docs"));
-        testCase.endOfStep("TS Navigate to docs");
+        testCase.endOfStep("Navigate to docs");
         await _wait( 5000, async () => (await driver.getAllWindowHandles()).length === 2);
         await driver.switchTo().window((await driver.getAllWindowHandles())[1]);
         await _highlight(_code("npm init"));
--- packages/e2e/e2e-suite/sakuli.io/typed-check.ts
+++ packages/e2e/e2e-suite/sakuli.io/typed-check.ts
@@ -6,20 +6,18 @@
         return await env.isDarwin() ? Key.CMD : Key.CTRL;
     }
     try {
-        await _navigateTo("https://sakuli.io");
-        testCase.endOfStep("TS Open Landing Page");
-        await _click(_link("EXPLORE"));
-        testCase.endOfStep("TS Open Explore page");
-        await _pageIsStable();
+        await _navigateTo("https://sakuli.io/contact/");
+        await _assertExists(_heading2("Get In Touch"));
+        testCase.endOfStep("TS Open Contact Page");
         await _click(_link("Docs"));
         testCase.endOfStep("TS Navigate to docs");
         await _wait( 5000, async () => (await driver.getAllWindowHandles()).length === 2);
         await driver.switchTo().window((await driver.getAllWindowHandles())[1]);
         await _highlight(_code("npm init"));
-        testCase.endOfStep("Find npm init code sample");
+        testCase.endOfStep("TS Find npm init code sample");
         await env.type(Key.W, await getControlKey());
         await driver.switchTo().window((await driver.getAllWindowHandles())[0]);
-        testCase.endOfStep("Close Tab");
+        testCase.endOfStep("TS Close Tab");
     } catch (e) {
         await testCase.handleException(e);
     } finally {
