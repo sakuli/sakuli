f22ea9df1ae28ba7b693c8effd01c2b520cef70a
Van Cliff Le <cliff.le.van@consol.de>
2020-04-15 12:26:51 +0200

    (#340) changed start of testcase back

--- packages/e2e/e2e-suite/sakuli.io/check.js
+++ packages/e2e/e2e-suite/sakuli.io/check.js
@@ -1,20 +1,21 @@
 (async () => {
     const testCase = new TestCase("My Typescript based test");
     try {
-        await _navigateTo("https://sakuli.io/contact/");
-        await _assertExists(_heading2("Get In Touch"));
-        testCase.endOfStep("Open Contact Page");
+        await _navigateTo("https://sakuli.io");
+        testCase.endOfStep("Open Landing Page");
+        await _click(_link("EXPLORE"));
+        testCase.endOfStep("Open Explore page");
+        await _pageIsStable();
         await _click(_link("Docs"));
         testCase.endOfStep("Navigate to docs");
         await _wait( 5000, async () => (await driver.getAllWindowHandles()).length === 2);
         await driver.switchTo().window((await driver.getAllWindowHandles())[1]);
         await _highlight(_code("npm init"));
-        testCase.endOfStep("Find npm init code sample");
         await driver.close();
         await driver.switchTo().window((await driver.getAllWindowHandles())[0]);
-        testCase.endOfStep("Close Tab");
+        testCase.endOfStep("Find npm init code sample");
     } catch (e) {
-        await testCase.handleException(e);
+        testCase.handleException(e);
     } finally {
         testCase.saveResult();
     }
--- packages/e2e/e2e-suite/sakuli.io/typed-check.ts
+++ packages/e2e/e2e-suite/sakuli.io/typed-check.ts
@@ -1,20 +1,21 @@
 (async () => {
     const testCase: TestCase = new TestCase("My Typescript based test");
     try {
-        await _navigateTo("https://sakuli.io/contact/");
-        await _assertExists(_heading2("Get In Touch"));
-        testCase.endOfStep("TS Open Contact Page");
+        await _navigateTo("https://sakuli.io");
+        testCase.endOfStep("TS Open Landing Page");
+        await _click(_link("EXPLORE"));
+        testCase.endOfStep("TS Open Explore page");
+        await _pageIsStable();
         await _click(_link("Docs"));
         testCase.endOfStep("TS Navigate to docs");
         await _wait( 5000, async () => (await driver.getAllWindowHandles()).length === 2);
         await driver.switchTo().window((await driver.getAllWindowHandles())[1]);
         await _highlight(_code("npm init"));
-        testCase.endOfStep("TS Find npm init code sample");
         await driver.close();
         await driver.switchTo().window((await driver.getAllWindowHandles())[0]);
-        testCase.endOfStep("TS Close Tab");
+        testCase.endOfStep("TS Find npm init code sample");
     } catch (e) {
-        await testCase.handleException(e);
+        testCase.handleException(e);
     } finally {
         testCase.saveResult();
     }
