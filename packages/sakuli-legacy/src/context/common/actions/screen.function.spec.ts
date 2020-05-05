28360bf70509171129e907a0e3c3317a36b5501c
Simon Hofmann <simon.hofmann@consol.de>
2020-05-05 10:50:38 +0200

    (#167) Added highlight function to ScreenApi

--- packages/sakuli-legacy/src/context/common/actions/screen.function.spec.ts
+++ packages/sakuli-legacy/src/context/common/actions/screen.function.spec.ts
@@ -1,11 +1,12 @@
-import {Region, screen} from "@nut-tree/nut-js";
-import {getTimestamp, ScreenApi} from "./screen.function";
-import {createRegionClass} from "../region";
-import {Project, TestExecutionContext} from "@sakuli/core";
-import {SimpleLogger} from "@sakuli/commons";
-import {FileType} from "@nut-tree/nut-js/dist/lib/file-type.enum";
-import {cwd} from "process";
-import {join} from "path";
+import { Region, screen } from "@nut-tree/nut-js";
+import { getTimestamp, ScreenApi } from "./screen.function";
+import { createRegionClass } from "../region";
+import { Project, TestExecutionContext } from "@sakuli/core";
+import { SimpleLogger } from "@sakuli/commons";
+import { FileType } from "@nut-tree/nut-js/dist/lib/file-type.enum";
+import { cwd } from "process";
+import { join } from "path";
+import { toNutRegion } from "./converter.function";
 
 let ctx: TestExecutionContext;
 let project: Project;
@@ -17,142 +18,204 @@ const testTimeout = 500;
 const expectedRegion = new Region(0, 0, 100, 100);
 
 beforeEach(() => {
-    ctx = new TestExecutionContext(new SimpleLogger());
-    project = new Project("");
+  ctx = new TestExecutionContext(new SimpleLogger());
+  project = new Project("");
 });
 
 describe("ScreenApi", () => {
-    it("should call `screen.find` with suitable Region", async () => {
-        // GIVEN
-        const TestRegion = createRegionClass(ctx, project);
-        const testRegion = new TestRegion(0, 0, 100, 100);
-        screen.find = jest.fn(() => Promise.resolve(expectedRegion));
+  it("should call `screen.find` with suitable Region", async () => {
+    // GIVEN
+    const TestRegion = createRegionClass(ctx, project);
+    const testRegion = new TestRegion(0, 0, 100, 100);
+    screen.find = jest.fn(() => Promise.resolve(expectedRegion));
 
-        // WHEN
-        await ScreenApi.find(testFilename, testPath, testConfidence, testRegion);
+    // WHEN
+    await ScreenApi.find(testFilename, testPath, testConfidence, testRegion);
 
-        // THEN
-        expect(screen.find).toBeCalledTimes(1);
-        expect(screen.find).toBeCalledWith(testFilename, {
-            confidence: testConfidence,
-            searchRegion: expectedRegion
-        });
+    // THEN
+    expect(screen.find).toBeCalledTimes(1);
+    expect(screen.find).toBeCalledWith(testFilename, {
+      confidence: testConfidence,
+      searchRegion: expectedRegion,
     });
+  });
 
-    it("should call `screen.waitFor` with suitable Region", async () => {
-        // GIVEN
-        const TestRegion = createRegionClass(ctx, project);
-        const testRegion = new TestRegion(0, 0, 100, 100);
-        screen.waitFor = jest.fn(() => Promise.resolve(expectedRegion));
+  it("should call `screen.waitFor` with suitable Region", async () => {
+    // GIVEN
+    const TestRegion = createRegionClass(ctx, project);
+    const testRegion = new TestRegion(0, 0, 100, 100);
+    screen.waitFor = jest.fn(() => Promise.resolve(expectedRegion));
 
-        // WHEN
-        await ScreenApi.waitForImage(testFilename, testPath, testConfidence, testTimeout, testRegion);
+    // WHEN
+    await ScreenApi.waitForImage(
+      testFilename,
+      testPath,
+      testConfidence,
+      testTimeout,
+      testRegion
+    );
 
-        // THEN
-        expect(screen.waitFor).toBeCalledTimes(1);
-        expect(screen.waitFor).toBeCalledWith(testFilename, testTimeout, {
-            confidence: testConfidence,
-            searchRegion: expectedRegion
-        });
+    // THEN
+    expect(screen.waitFor).toBeCalledTimes(1);
+    expect(screen.waitFor).toBeCalledWith(testFilename, testTimeout, {
+      confidence: testConfidence,
+      searchRegion: expectedRegion,
     });
+  });
+
+  it("should call `screen.highlight` with suitable Region and timeout", async () => {
+    // GIVEN
+    const TestRegion = createRegionClass(ctx, project);
+    const testRegion = new TestRegion(0, 0, 100, 100);
+    const expectedRegion = await toNutRegion(testRegion);
+    const testHighlightDuration = 5;
+    screen.highlight = jest.fn(() => Promise.resolve(expectedRegion));
+
+    // WHEN
+    await ScreenApi.highlight(testRegion, testHighlightDuration);
+
+    // THEN
+    expect(screen.highlight).toBeCalledTimes(1);
+    expect(screen.highlight).toBeCalledWith(expectedRegion);
+  });
+
+  it("should return passed region after calling highlight for method chaining", async () => {
+    // GIVEN
+    const TestRegion = createRegionClass(ctx, project);
+    const testRegion = new TestRegion(0, 0, 100, 100);
+    const testHighlightDuration = 5;
+    screen.highlight = jest.fn(() => Promise.resolve(expectedRegion));
+
+    // WHEN
+    const result = await ScreenApi.highlight(testRegion, testHighlightDuration);
+
+    // THEN
+    expect(result).toEqual(testRegion);
+  });
 });
 
 describe("takeScreenshot*", () => {
-    it("should call `screen.capture` with fallback `cwd()`", async () => {
-        // GIVEN
-        const screenShotFileName = "screenshot";
-        const screenShotFileExt = ".png";
-        screen.capture = jest.fn(() => Promise.resolve("filename"));
+  it("should call `screen.capture` with fallback `cwd()`", async () => {
+    // GIVEN
+    const screenShotFileName = "screenshot";
+    const screenShotFileExt = ".png";
+    screen.capture = jest.fn(() => Promise.resolve("filename"));
 
-        // WHEN
-        await ScreenApi.takeScreenshot(`${screenShotFileName}${screenShotFileExt}`);
+    // WHEN
+    await ScreenApi.takeScreenshot(`${screenShotFileName}${screenShotFileExt}`);
 
-        // THEN
-        expect(screen.capture).toBeCalledTimes(1);
-        expect(screen.capture).toBeCalledWith(screenShotFileName, FileType.PNG, cwd());
-    });
+    // THEN
+    expect(screen.capture).toBeCalledTimes(1);
+    expect(screen.capture).toBeCalledWith(
+      screenShotFileName,
+      FileType.PNG,
+      cwd()
+    );
+  });
 
-    it("should call `screen.capture` with custom path", async () => {
-        // GIVEN
-        const screenShotPath = join("test", "path", "to");
-        const screenShotFileName = "screenshot";
-        const screenShotFileExt = ".png";
-        screen.capture = jest.fn(() => Promise.resolve("filename"));
+  it("should call `screen.capture` with custom path", async () => {
+    // GIVEN
+    const screenShotPath = join("test", "path", "to");
+    const screenShotFileName = "screenshot";
+    const screenShotFileExt = ".png";
+    screen.capture = jest.fn(() => Promise.resolve("filename"));
 
-        // WHEN
-        await ScreenApi.takeScreenshot(`${join(screenShotPath, screenShotFileName)}${screenShotFileExt}`);
+    // WHEN
+    await ScreenApi.takeScreenshot(
+      `${join(screenShotPath, screenShotFileName)}${screenShotFileExt}`
+    );
 
-        // THEN
-        expect(screen.capture).toBeCalledTimes(1);
-        expect(screen.capture).toBeCalledWith(screenShotFileName, FileType.PNG, screenShotPath);
-    });
+    // THEN
+    expect(screen.capture).toBeCalledTimes(1);
+    expect(screen.capture).toBeCalledWith(
+      screenShotFileName,
+      FileType.PNG,
+      screenShotPath
+    );
+  });
 
-    it("should call `screen.capture` with timestamp and fallback `cwd()`", async () => {
-        // GIVEN
-        const screenShotFileName = "screenshot";
-        const screenShotFileExt = ".png";
-        screen.capture = jest.fn(() => Promise.resolve("filename"));
+  it("should call `screen.capture` with timestamp and fallback `cwd()`", async () => {
+    // GIVEN
+    const screenShotFileName = "screenshot";
+    const screenShotFileExt = ".png";
+    screen.capture = jest.fn(() => Promise.resolve("filename"));
 
-        // WHEN
-        await ScreenApi.takeScreenshotWithTimestamp(`${screenShotFileName}${screenShotFileExt}`);
+    // WHEN
+    await ScreenApi.takeScreenshotWithTimestamp(
+      `${screenShotFileName}${screenShotFileExt}`
+    );
 
-        // THEN
-        expect(screen.capture).toBeCalledTimes(1);
-        expect(screen.capture).toBeCalledWith(screenShotFileName, FileType.PNG, cwd(), expect.stringMatching(/\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}_/), "");
-    });
+    // THEN
+    expect(screen.capture).toBeCalledTimes(1);
+    expect(screen.capture).toBeCalledWith(
+      screenShotFileName,
+      FileType.PNG,
+      cwd(),
+      expect.stringMatching(/\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}_/),
+      ""
+    );
+  });
 
-    it("should call `screen.capture` with timestamp and custom path", async () => {
-        // GIVEN
-        const screenShotPath = join("test", "path", "to");
-        const screenShotFileName = "screenshot";
-        const screenShotFileExt = ".png";
-        screen.capture = jest.fn(() => Promise.resolve("filename"));
+  it("should call `screen.capture` with timestamp and custom path", async () => {
+    // GIVEN
+    const screenShotPath = join("test", "path", "to");
+    const screenShotFileName = "screenshot";
+    const screenShotFileExt = ".png";
+    screen.capture = jest.fn(() => Promise.resolve("filename"));
 
-        // WHEN
-        await ScreenApi.takeScreenshotWithTimestamp(`${join(screenShotPath, screenShotFileName)}${screenShotFileExt}`);
+    // WHEN
+    await ScreenApi.takeScreenshotWithTimestamp(
+      `${join(screenShotPath, screenShotFileName)}${screenShotFileExt}`
+    );
 
-        // THEN
-        expect(screen.capture).toBeCalledTimes(1);
-        expect(screen.capture).toBeCalledWith(screenShotFileName, FileType.PNG, screenShotPath, expect.stringMatching(/\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}_/), "");
-    });
+    // THEN
+    expect(screen.capture).toBeCalledTimes(1);
+    expect(screen.capture).toBeCalledWith(
+      screenShotFileName,
+      FileType.PNG,
+      screenShotPath,
+      expect.stringMatching(/\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2}_/),
+      ""
+    );
+  });
 });
 
 describe("getTimestamp", () => {
-    it("should output an ISO string for current timezone", () => {
-        // GIVEN
-        const timestamp = 1562688075714;
-        const offset = -120 * 60_000;
+  it("should output an ISO string for current timezone", () => {
+    // GIVEN
+    const timestamp = 1562688075714;
+    const offset = -120 * 60_000;
 
-        // WHEN
-        const result = getTimestamp(timestamp, offset);
+    // WHEN
+    const result = getTimestamp(timestamp, offset);
 
-        // THEN
-        expect(result).not.toContain(":");
-        expect(result).toBe("2019-07-09T18-01-15")
-    });
+    // THEN
+    expect(result).not.toContain(":");
+    expect(result).toBe("2019-07-09T18-01-15");
+  });
 
-    it("should output an ISO string for UTC", () => {
-        // GIVEN
-        const timestamp = 1562688075714;
+  it("should output an ISO string for UTC", () => {
+    // GIVEN
+    const timestamp = 1562688075714;
 
-        // WHEN
-        const result = getTimestamp(timestamp);
+    // WHEN
+    const result = getTimestamp(timestamp);
 
-        // THEN
-        expect(result).not.toContain(":");
-        expect(result).toBe("2019-07-09T16-01-15")
-    });
+    // THEN
+    expect(result).not.toContain(":");
+    expect(result).toBe("2019-07-09T16-01-15");
+  });
 
-    it("should output current UTC by default", () => {
-        // GIVEN
-        const timestamp = 1562688075714;
-        Date.now = jest.fn(() => timestamp);
+  it("should output current UTC by default", () => {
+    // GIVEN
+    const timestamp = 1562688075714;
+    Date.now = jest.fn(() => timestamp);
 
-        // WHEN
-        const result = getTimestamp();
+    // WHEN
+    const result = getTimestamp();
 
-        // THEN
-        expect(result).not.toContain(":");
-        expect(result).toBe("2019-07-09T16-01-15")
-    });
+    // THEN
+    expect(result).not.toContain(":");
+    expect(result).toBe("2019-07-09T16-01-15");
+  });
 });
--- packages/sakuli-legacy/src/context/common/actions/screen.function.ts
+++ packages/sakuli-legacy/src/context/common/actions/screen.function.ts
@@ -1,23 +1,24 @@
-import {Region as NutRegion, screen} from "@nut-tree/nut-js";
-import {FileType} from "@nut-tree/nut-js/dist/lib/file-type.enum";
-import {parse} from "path";
-import {cwd} from "process";
-import {Region} from "../region";
+import { Region as NutRegion, screen } from "@nut-tree/nut-js";
+import { FileType } from "@nut-tree/nut-js/dist/lib/file-type.enum";
+import { parse } from "path";
+import { cwd } from "process";
+import { Region } from "../region";
+import { toNutRegion } from "./converter.function";
 
 const getCoordinates = async (region: Region) => {
-    return ({
-        left: await region.getX() || 0,
-        top: await region.getY() || 0,
-        width: await region.getW() || await screen.width(),
-        height: await region.getH() || await screen.height(),
-    })
+  return {
+    left: (await region.getX()) || 0,
+    top: (await region.getY()) || 0,
+    width: (await region.getW()) || (await screen.width()),
+    height: (await region.getH()) || (await screen.height()),
+  };
 };
 
 /**
  * getTimezoneOffset returns the timezone offset to UTC in milliseconds
  */
 const getTimezoneOffset = (): number => {
-    return new Date().getTimezoneOffset() * 60_000;
+  return new Date().getTimezoneOffset() * 60_000;
 };
 
 /**
@@ -25,80 +26,112 @@ const getTimezoneOffset = (): number => {
  * @param when Timestamp in milliseconds
  * @param offset Timezone offset in milliseconds
  */
-export const getTimestamp = (when: number = Date.now(), offset: number = 0): string => {
-    const timestamp = new Date(when - offset).toISOString();
-    const sliceIndex = timestamp.indexOf('.');
-    const timestampString = (sliceIndex > -1) ? timestamp.slice(0, sliceIndex) : timestamp;
-    return timestampString.split(":").join("-");
+export const getTimestamp = (
+  when: number = Date.now(),
+  offset: number = 0
+): string => {
+  const timestamp = new Date(when - offset).toISOString();
+  const sliceIndex = timestamp.indexOf(".");
+  const timestampString =
+    sliceIndex > -1 ? timestamp.slice(0, sliceIndex) : timestamp;
+  return timestampString.split(":").join("-");
 };
 
-export type SearchResult = { left: number, top: number, width: number, height: number };
+export type SearchResult = {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+};
 
 export const ScreenApi = {
-    async find(filename: string, path: string, confidence: number, searchRegion: Region): Promise<SearchResult> {
-        const {left, top, width, height} = await getCoordinates(searchRegion);
-        return new Promise<SearchResult>(async (resolve, reject) => {
-            try {
-                screen.config.resourceDirectory = path;
-                const result = await screen.find(filename, {
-                    confidence,
-                    searchRegion: new NutRegion(
-                        left,
-                        top,
-                        width,
-                        height
-                    )
-                });
-                resolve({
-                    left: result.left,
-                    top: result.top,
-                    width: result.width,
-                    height: result.height
-                });
-            } catch (e) {
-                reject(e);
-            }
+  async find(
+    filename: string,
+    path: string,
+    confidence: number,
+    searchRegion: Region
+  ): Promise<SearchResult> {
+    const { left, top, width, height } = await getCoordinates(searchRegion);
+    return new Promise<SearchResult>(async (resolve, reject) => {
+      try {
+        screen.config.resourceDirectory = path;
+        const result = await screen.find(filename, {
+          confidence,
+          searchRegion: new NutRegion(left, top, width, height),
         });
-    },
-    async waitForImage(filepath: string, path: string, confidence: number, timeoutMs: number, searchRegion: Region): Promise<SearchResult> {
-        const {left, top, width, height} = await getCoordinates(searchRegion);
-        return new Promise<SearchResult>(async (resolve, reject) => {
-            try {
-                screen.config.resourceDirectory = path;
-                const result = await screen.waitFor(filepath, timeoutMs, {
-                    confidence,
-                    searchRegion: new NutRegion(
-                        left,
-                        top,
-                        width,
-                        height
-                    )
-                });
-                resolve({
-                    left: result.left,
-                    top: result.top,
-                    width: result.width,
-                    height: result.height
-                });
-            } catch (e) {
-                reject(e);
-            }
+        resolve({
+          left: result.left,
+          top: result.top,
+          width: result.width,
+          height: result.height,
         });
-    },
-    async height(): Promise<number> {
-        return screen.height();
-    },
-    async width(): Promise<number> {
-        return screen.width();
-    },
-    async takeScreenshot(filename: string): Promise<string> {
-        const pathParts = parse(filename);
-        const outputDir = (pathParts.dir && pathParts.dir.length > 0) ? pathParts.dir : cwd();
-        return screen.capture(pathParts.name, FileType.PNG, outputDir);
-    },
-    async takeScreenshotWithTimestamp(filename: string): Promise<string> {
-        const pathParts = parse(filename);
-        const outputDir = (pathParts.dir && pathParts.dir.length > 0) ? pathParts.dir : cwd();
-        return screen.capture(pathParts.name, FileType.PNG, outputDir, `${getTimestamp(Date.now(), getTimezoneOffset())}_`, "");
-    }
+      } catch (e) {
+        reject(e);
+      }
+    });
+  },
+  async waitForImage(
+    filepath: string,
+    path: string,
+    confidence: number,
+    timeoutMs: number,
+    searchRegion: Region
+  ): Promise<SearchResult> {
+    const { left, top, width, height } = await getCoordinates(searchRegion);
+    return new Promise<SearchResult>(async (resolve, reject) => {
+      try {
+        screen.config.resourceDirectory = path;
+        const result = await screen.waitFor(filepath, timeoutMs, {
+          confidence,
+          searchRegion: new NutRegion(left, top, width, height),
+        });
+        resolve({
+          left: result.left,
+          top: result.top,
+          width: result.width,
+          height: result.height,
+        });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  },
+  async highlight(
+    regionToHighlight: Region,
+    duration: number
+  ): Promise<Region> {
+    return new Promise<Region>(async (resolve, reject) => {
+      try {
+        screen.config.highlightDurationMs = duration * 1000;
+        await screen.highlight(await toNutRegion(regionToHighlight));
+        resolve(regionToHighlight);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  },
+  async height(): Promise<number> {
+    return screen.height();
+  },
+  async width(): Promise<number> {
+    return screen.width();
+  },
+  async takeScreenshot(filename: string): Promise<string> {
+    const pathParts = parse(filename);
+    const outputDir =
+      pathParts.dir && pathParts.dir.length > 0 ? pathParts.dir : cwd();
+    return screen.capture(pathParts.name, FileType.PNG, outputDir);
+  },
+  async takeScreenshotWithTimestamp(filename: string): Promise<string> {
+    const pathParts = parse(filename);
+    const outputDir =
+      pathParts.dir && pathParts.dir.length > 0 ? pathParts.dir : cwd();
+    return screen.capture(
+      pathParts.name,
+      FileType.PNG,
+      outputDir,
+      `${getTimestamp(Date.now(), getTimezoneOffset())}_`,
+      ""
+    );
+  },
 };
