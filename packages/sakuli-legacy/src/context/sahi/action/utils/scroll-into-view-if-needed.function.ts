0b515871f13444fe7ef61ad67b8bd759c0b026ab
Simon Hofmann <simon.hofmann@consol.de>
2020-05-08 13:07:29 +0200

    (#414) Added docstring to scrollIntoView explaining it's inner working

--- packages/sakuli-legacy/src/context/sahi/action/utils/scroll-into-view-if-needed.function.ts
+++ packages/sakuli-legacy/src/context/sahi/action/utils/scroll-into-view-if-needed.function.ts
@@ -1,14 +1,37 @@
-import {WebElement} from "selenium-webdriver";
-import {TestExecutionContext} from "@sakuli/core";
+import { WebElement } from "selenium-webdriver";
+import { TestExecutionContext } from "@sakuli/core";
 
-export async function scrollIntoViewIfNeeded(element: WebElement, ctx: TestExecutionContext): Promise<boolean> {
-    if (!element) {
-        ctx.logger.debug("scroll into view failed: element was null or undefined");
-        return false;
-    }
-    ctx.logger.trace(`scroll into view started with element: ${JSON.stringify(element)}`);
-    try {
-        const returnValue = await element.getDriver().executeAsyncScript<boolean>(`
+/**
+ * scrollIntoViewIfNeeded will scroll a given {@link WebElement} into view to perform further actions on it.
+ * This is necessary to avoid errors due to elements not being in the current viewport.
+ *
+ * scrollIntoViewIfNeeded solves this problem by executing a script which calls scrollIntoView on a given element.
+ * Additionally, it will register a timeout (isScrollingTimeout) to handle
+ *
+ * - no scrolling at all, so the script will resolve after an initial delay
+ * - continuous scrolling, which will continuously update the timeout as long as we're still scrolling
+ *
+ * The reasoning for this behaviour is the fact that scrollIntoView resolves immediately,
+ * without taking into account the time the browser take to actually scroll the viewport.
+ * By adding an eventlistener to the browser's 'scroll' event, we're manually waiting for scrolling to end via said timeout
+ *
+ * @param element The {@link WebElement} to scroll into view
+ * @param ctx The current {@link TestExecutionContext}
+ */
+export async function scrollIntoViewIfNeeded(
+  element: WebElement,
+  ctx: TestExecutionContext
+): Promise<boolean> {
+  if (!element) {
+    ctx.logger.debug("scroll into view failed: element was null or undefined");
+    return false;
+  }
+  ctx.logger.trace(
+    `scroll into view started with element: ${JSON.stringify(element)}`
+  );
+  try {
+    const returnValue = await element.getDriver().executeAsyncScript<boolean>(
+      `
         const __done__ = arguments[arguments.length - 1];
         let isScrollingTimeout = setTimeout(() => resolve(false), 200);
         
@@ -28,11 +51,21 @@ export async function scrollIntoViewIfNeeded(element: WebElement, ctx: TestExecu
             inline: "center",
             block: "center"
         });
-    `, element);
-        ctx.logger.trace(`scroll into view finished for element: ${JSON.stringify(element)} with value ${returnValue}`);
-        return returnValue;
-    } catch (e) {
-        ctx.logger.trace(`Caught ${e} when trying to scroll element: ${JSON.stringify(element)} into view`);
-        return false;
-    }
+    `,
+      element
+    );
+    ctx.logger.trace(
+      `scroll into view finished for element: ${JSON.stringify(
+        element
+      )} with value ${returnValue}`
+    );
+    return returnValue;
+  } catch (e) {
+    ctx.logger.trace(
+      `Caught ${e} when trying to scroll element: ${JSON.stringify(
+        element
+      )} into view`
+    );
+    return false;
+  }
 }
