13a853de8ec4b4bf896a22a11d0f402d2c2c6cee
Van Cliff Le <cliff.le.van@consol.de>
2020-05-10 22:48:00 +0200

    (#275) added test for get-sakuli-presets

---
+++ packages/sakuli-core/src/bootstrap/get-sakuli-presets.function.spec.ts
@@ -0,0 +1,30 @@
+import mockFs from "mock-fs";
+import { getSakuliPresets } from "./get-sakuli-presets.function";
+
+describe("get-sakuli-presets", () => {
+  beforeEach(() => {});
+
+  it("should return execa package name", () => {
+    mockFs({
+      "/base": {
+        execa: {
+          "package.json":
+            '{ "name": "sakuli", "keywords": [ "sakuliPreset" ] }',
+        },
+        foo: {
+          "package.json": '{ "name": "just another package json" }',
+        },
+        "empty-dir": {},
+      },
+    });
+
+    const sakuliPresets = getSakuliPresets(["/base"]);
+
+    expect(sakuliPresets).toEqual(["sakuli"]);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    mockFs.restore();
+  });
+});
