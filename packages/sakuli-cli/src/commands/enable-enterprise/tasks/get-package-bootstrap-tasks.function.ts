39ef47624480e121739c8ec5db541d5b6f98fd8b
Van Cliff Le <cliff.le.van@consol.de>
2020-05-11 08:27:22 +0200

    (#275) remove preset configuration from bootstrap command

--- packages/sakuli-cli/src/commands/enable-enterprise/tasks/get-package-bootstrap-tasks.function.ts
+++ packages/sakuli-cli/src/commands/enable-enterprise/tasks/get-package-bootstrap-tasks.function.ts
@@ -1,7 +1,6 @@
-import { Task, oraTask, installPackageTask, ConfigurationRecord } from ".";
+import { ConfigurationRecord, installPackageTask, oraTask, Task } from ".";
 import chalk from "chalk";
 import { configureFeatureTask } from "./configure-feature-task.function";
-import { registerPackageTask } from "./register-package-task.function";
 
 /**
  * Creates a list of {@see oraTask} which can be used to bootstrap configuration and installation of a certain package.
@@ -9,10 +8,15 @@ import { registerPackageTask } from "./register-package-task.function";
  * @param configurationItems
  */
 export const getPackageBootstrapTasks = (
-    npmPackageName: string,
-    configurationItems: ConfigurationRecord
+  npmPackageName: string,
+  configurationItems: ConfigurationRecord
 ): Task[] => [
-        oraTask(chalk`Installing {bold ${npmPackageName}}`, installPackageTask(npmPackageName)),
-        oraTask(chalk`Bootstrapping configuration for {bold ${npmPackageName}}`, configureFeatureTask(configurationItems)),
-        oraTask(chalk`Registering {bold ${npmPackageName}} in project`, registerPackageTask(npmPackageName))
-    ]
+  oraTask(
+    chalk`Installing {bold ${npmPackageName}}`,
+    installPackageTask(npmPackageName)
+  ),
+  oraTask(
+    chalk`Bootstrapping configuration for {bold ${npmPackageName}}`,
+    configureFeatureTask(configurationItems)
+  ),
+];
