4cdcfa63d40de53d01a799bc41b2cdbd9f4cac79
Simon Hofmann <simon.hofmann@consol.de>
2020-05-19 18:24:06 +0200

    (#167) Additional test to verify default values used when converting regions with no defined bounds

--- packages/sakuli-legacy/src/context/common/actions/converter.function.spec.ts
+++ packages/sakuli-legacy/src/context/common/actions/converter.function.spec.ts
@@ -1,5 +1,5 @@
 import { createRegionClass } from "../region";
-import { Region as NutRegion } from "@nut-tree/nut-js/dist/lib/region.class";
+import { screen, Region as NutRegion } from "@nut-tree/nut-js";
 import { Project, TestExecutionContext } from "@sakuli/core";
 import { createTestExecutionContextMock } from "../../__mocks__";
 import { toNutRegion } from "./converter.function";
@@ -21,6 +21,24 @@ describe("toNutRegion", () => {
     const height = 40;
     const sakuliRegion = new SakuliRegion(left, top, width, height);
     const nutRegion = new NutRegion(left, top, width, height);
+
+    // WHEN
+    const convertedRegion = await toNutRegion(sakuliRegion);
+
+    // THEN
+    expect(convertedRegion).toEqual(nutRegion);
+  });
+
+  it("should use screen bounds as defaults for nut.js regions", async () => {
+    // GIVEN
+    const SakuliRegion = createRegionClass(ctx, project);
+    const sakuliRegion = new SakuliRegion();
+    const nutRegion = new NutRegion(
+      0,
+      0,
+      await screen.width(),
+      await screen.height()
+    );
 
     // WHEN
     const convertedRegion = await toNutRegion(sakuliRegion);
