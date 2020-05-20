2862a27bba7e2ba1b156d78058f3b4f1d5735b8b
Sven Hettwer <sven.hettwer@consol.de>
2020-05-20 15:30:37 +0200

    (#276) reduced cognitive complexity... hopefully

--- packages/sakuli-core/src/bootstrap/get-sakuli-presets.function.ts
+++ packages/sakuli-core/src/bootstrap/get-sakuli-presets.function.ts
@@ -7,36 +7,36 @@ export function getSakuliPresets(nodeModulesPaths: string[]): string[] {
   let sakuliPresets: string[] = [];
 
   for (const nodeModulesPath of nodeModulesPaths) {
-    const candidates: string[] = [];
-    readdirSync(nodeModulesPath).forEach((name) => {
-      const dir = join(nodeModulesPath, name);
-      if (
-        name.startsWith("@sakuli") &&
-        isExistingDirectory(join(nodeModulesPath, name))
-      ) {
-        readdirSync(dir).forEach((n) => {
-          candidates.push(join(name, n));
-        });
-      }
-      candidates.push(name);
-    });
-
-    candidates.forEach((name) => {
-      const dependencyInfoFile = join(nodeModulesPath, name, "package.json");
-      if (!existsSync(dependencyInfoFile)) {
-        return;
-      }
-
-      const dependency = JSON.parse(readFileSync(dependencyInfoFile, "utf-8"));
-      if (
-        isSakuliPreset(dependency) &&
-        !sakuliPresets.includes(dependency) &&
-        dependency?.name
-      ) {
-        sakuliPresets.push(dependency.name);
-      }
-    });
+    const sakuliOrgPackages = getSakuliOrgPackages(nodeModulesPath);
+    const foundSakuliPresets = getSakuliPresetsFromPackages(sakuliOrgPackages);
+    sakuliPresets.push(
+      ...foundSakuliPresets.filter(
+        (foundSakuliPreset) => !sakuliPresets.includes(foundSakuliPreset)
+      )
+    );
   }
 
   return sakuliPresets;
+}
+
+function getSakuliOrgPackages(nodeModulesPath: string) {
+  const sakuliOrgDir = join(nodeModulesPath, "@sakuli");
+  if (isExistingDirectory(sakuliOrgDir)) {
+    return readdirSync(sakuliOrgDir)
+      .map((sakuliOrgPackage) => join(sakuliOrgDir, sakuliOrgPackage))
+      .filter(isExistingDirectory);
+  }
+  return [];
+}
+
+function getSakuliPresetsFromPackages(sakuliOrgPackages: string[]) {
+  return sakuliOrgPackages
+    .map((name) => join(name, "package.json"))
+    .filter(existsSync)
+    .map((packageJsonFile) =>
+      JSON.parse(readFileSync(packageJsonFile, "utf-8"))
+    )
+    .filter(isSakuliPreset)
+    .filter((packageJson) => packageJson?.name)
+    .map((packageJson) => <string>packageJson.name);
 }
