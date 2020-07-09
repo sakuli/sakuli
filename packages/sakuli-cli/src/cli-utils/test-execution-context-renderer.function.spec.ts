ab04ed2c1a718157aa5cfdcbf4af83723a24c834
YunaAmnell <marlene.kermani@consol.de>
2020-07-09 12:52:33 +0200

    (#338) changed tests

--- packages/sakuli-cli/src/cli-utils/test-execution-context-renderer.function.spec.ts
+++ packages/sakuli-cli/src/cli-utils/test-execution-context-renderer.function.spec.ts
@@ -1,42 +1,71 @@
-import { testExecutionContextRenderer, logEntityOnStart, logEntityOnEnd } from "./test-execution-context-renderer.function";
+import {
+  testExecutionContextRenderer,
+  entityOnStartLogMessage,
+  entityOnEndLogMessage,
+} from "./test-execution-context-renderer.function";
 import { TestExecutionContext, TestContextEntity } from "@sakuli/core";
 import { SimpleLogger } from "@sakuli/commons";
-import {mockPartial} from "sneer";
-
+import { mockPartial } from "sneer";
 
 describe("testExecutionContextRenderer", () => {
   it("should log info about starting tests", async () => {
-    const lg = new SimpleLogger();
-    lg.info = jest.fn();
-    const ctx = new TestExecutionContext(lg);
-    const rnd = testExecutionContextRenderer(ctx);
+    const logger = mockPartial<SimpleLogger>({
+      info: jest.fn(),
+    });
+    const ctx = new TestExecutionContext(logger);
+    const rendering = testExecutionContextRenderer(ctx);
     ctx.startExecution();
-    ctx.startTestSuite();
-    ctx.startTestCase();
+    ctx.startTestSuite({ id: "Testsuite" });
+    ctx.startTestCase({ id: "Testcase" });
+    ctx.startTestStep({ id: "Teststep" });
+    ctx.endTestStep();
     ctx.endTestCase();
     ctx.endTestSuite();
     ctx.endExecution();
-    await rnd;
+    await rendering;
 
-    expect(lg.info).toHaveBeenNthCalledWith(1, "Started Execution");
-    expect(lg.info).toHaveBeenNthCalledWith(2, "Started Testsuite UNNAMED");
+    expect(logger.info).toHaveBeenNthCalledWith(1, "Started Execution");
+    expect(logger.info).toHaveBeenNthCalledWith(
+      2,
+      "Started Testsuite Testsuite"
+    );
+    expect(logger.info).toHaveBeenNthCalledWith(3, "Started Testcase Testcase");
+    expect(logger.info).toHaveBeenNthCalledWith(
+      4,
+      expect.stringMatching(/Finished Step Teststep with state Ok \d.\d{3}s/)
+    );
+    expect(logger.info).toHaveBeenNthCalledWith(
+      5,
+      expect.stringMatching(
+        /Finished Testcase Testcase with state Ok \d.\d{3}s/
+      )
+    );
+    expect(logger.info).toHaveBeenNthCalledWith(
+      6,
+      expect.stringMatching(
+        /Finished Testsuite Testsuite with state Ok \d.\d{3}s/
+      )
+    );
+    expect(logger.info).toHaveBeenNthCalledWith(7, "Finished Execution");
   });
 });
 
 describe("logEntityOn functions", () => {
+  const tce = mockPartial<TestContextEntity>({
+    id: "123",
+    state: 4,
+    duration: 1,
+  });
 
-    const tce = mockPartial<TestContextEntity>({
-        id: "123",
-        state: 4,
-        duration: 1
-    });
+  const name = "name";
 
-    const name = "name"
+  it("should return logging output for entity on start", () => {
+    expect(entityOnStartLogMessage(tce, name)).toBe("Started name 123");
+  });
 
-    it("should return the right output with correct state", () => {
-
-    expect(logEntityOnStart(tce,name)).toBe("Started name 123");
-    expect(logEntityOnEnd(tce,name)).toBe("Finished name 123 with state Error 1s");
-    });
-})
-
+  it("should return logging output for entity on end", () => {
+    expect(entityOnEndLogMessage(tce, name)).toBe(
+      "Finished name 123 with state Error 1s"
+    );
+  });
+});
