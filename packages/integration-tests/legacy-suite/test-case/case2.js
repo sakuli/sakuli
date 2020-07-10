7bf7cc5e0b362171a344c7a87dec142ff7d56acc
Van Cliff Le <cliff.le.van@consol.de>
2020-07-10 11:00:11 +0200

    (#338) updated integration tests

--- packages/integration-tests/legacy-suite/test-case/case2.js
+++ packages/integration-tests/legacy-suite/test-case/case2.js
@@ -1,25 +1,21 @@
 (async () => {
   const testCase = new TestCase();
   const url = "https://sakuli.io";
-  const env = new Environment();
   try {
     await _navigateTo(url + "/enterprise");
     await testCase.endOfStep("Opened Enterprise page.");
-    if (await _exists(_link("Learn more"))) {
-      await testCase.endOfStep("Cookie Banner found");
-      await _click(_button("I agree"));
-    } else {
-      await testCase.endOfStep("Waited for ");
-    }
-    var links = await _collect("_link", /Contact/);
-    await _focus(links[1]);
-    await _highlight(links[1]);
-    await _click(links[1]);
-    await testCase.endOfStep("Contact form clicked.");
-    await _wait(3000);
+    await _click(_link(/Settings/, _in(_div("cookie-notice "))));
+    await _click(_button("Accept selected"));
+    await testCase.endOfStep("Accept essential cookies");
+    await _highlight(_select("powermail_field_salutation"));
+    await _setSelected(_select("powermail_field_salutation"), "Mrs.");
+    await testCase.endOfStep("Set salutation");
+    await _highlight(_checkbox("powermail_field_datenschutz_1"));
+    await _check(_checkbox("powermail_field_datenschutz_1"));
+    await testCase.endOfStep("Accept privacy policy");
   } catch (e) {
     await testCase.handleException(e);
   } finally {
     await testCase.saveResult();
   }
-})().then(done);
+})();
--- packages/integration-tests/legacy-suite/test-case/typed-case.ts
+++ packages/integration-tests/legacy-suite/test-case/typed-case.ts
@@ -1,12 +1,22 @@
 (async () => {
   const testCase: TestCase = new TestCase("My Typescript based test");
   try {
-    await _navigateTo("https://sakuli.io"); // 1
-    testCase.endOfStep("TS Open Landing Page", 5); // 2
-    await _click(_link("Getting started")); // 3
-    testCase.endOfStep("TS Navigate to Getting Started", 3); // 4
-    await _highlight(_code("npm init")); // 5
+    await _navigateTo("https://sakuli.io");
+    testCase.endOfStep("Open Landing Page", 100, 150);
+    await _highlight(_link("TAKE ME TO THE DOCS AND GET STARTED"));
+    await _click(_link("TAKE ME TO THE DOCS AND GET STARTED"));
+    await _wait(1000);
+    let handles = await driver.getAllWindowHandles();
+    Logger.logInfo(`Window handles: ${handles}`);
+    await driver.switchTo().window(handles[1]);
+    testCase.endOfStep("Navigate to Getting Started", 30, 100);
+    await _wait(5000);
+    await _highlight(_code("npm init"));
+    await driver.close();
     testCase.endOfStep("Find npm init code sample");
+    await driver.switchTo().window(handles[0]);
+    await _highlight(_link("Docs"));
+    testCase.endOfStep("End");
   } catch (e) {
     testCase.handleException(e);
   } finally {
