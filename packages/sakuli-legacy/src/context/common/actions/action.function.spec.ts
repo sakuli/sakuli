8b1046b03d8ba73fc37d967e0f5b5c9ed2959d5a
Van Cliff Le <cliff.le.van@consol.de>
2020-07-24 12:17:15 +0200

    (#338) applied suggestion from code review

--- packages/sakuli-legacy/src/context/common/actions/action.function.spec.ts
+++ packages/sakuli-legacy/src/context/common/actions/action.function.spec.ts
@@ -1,7 +1,7 @@
-import {runAsAction} from "./action.function";
-import {mockPartial} from "sneer";
-import {TestExecutionContext} from "@sakuli/core";
-import {SimpleLogger} from "@sakuli/commons";
+import { runAsAction } from "./action.function";
+import { mockPartial } from "sneer";
+import { TestExecutionContext } from "@sakuli/core";
+import { SimpleLogger } from "@sakuli/commons";
 
 function createTestExecutionContextMock() {
   return mockPartial<TestExecutionContext>({
@@ -9,13 +9,12 @@ function createTestExecutionContextMock() {
     endTestAction: jest.fn(),
     getCurrentTestAction: jest.fn(),
     logger: mockPartial<SimpleLogger>({
-      trace: jest.fn()
+      trace: jest.fn(),
     }),
   });
 }
 
 describe("create-with-action-context", () => {
-
   let ctx: TestExecutionContext;
   let withActionContext: any;
 
@@ -24,22 +23,25 @@ describe("create-with-action-context", () => {
   });
 
   it("should execute successfully", async () => {
-      withActionContext = runAsAction(ctx, "myAction", () => "worked");
-      await withActionContext();
+    withActionContext = runAsAction(ctx, "myAction", () => "worked");
+    await withActionContext();
 
-      expect(ctx.logger.trace).toHaveBeenCalledTimes(2);
-      expect(ctx.startTestAction).toHaveBeenCalled();
-      expect(ctx.endTestAction).toHaveBeenCalled();
+    expect(ctx.startTestAction).toHaveBeenCalled();
+    expect(ctx.logger.trace).toHaveBeenCalledTimes(2);
+    expect(ctx.endTestAction).toHaveBeenCalled();
   });
 
   it("should log an error if action did throw an error", async () => {
-      withActionContext = runAsAction(ctx, "myAction", () => {throw Error("myError");});
+    withActionContext = runAsAction(ctx, "myAction", () => {
+      throw Error("myError");
+    });
 
-      await expect(withActionContext()).rejects.toThrow("Error in action: myAction \nmyError");
+    await expect(withActionContext()).rejects.toThrow(
+      "Error in action: myAction \nmyError"
+    );
 
-      expect(ctx.logger.trace).toHaveBeenCalledTimes(2);
-      expect(ctx.startTestAction).toHaveBeenCalled();
-      expect(ctx.endTestAction).toHaveBeenCalled();
+    expect(ctx.startTestAction).toHaveBeenCalled();
+    expect(ctx.logger.trace).toHaveBeenCalledTimes(2);
+    expect(ctx.endTestAction).toHaveBeenCalled();
   });
-
 });
