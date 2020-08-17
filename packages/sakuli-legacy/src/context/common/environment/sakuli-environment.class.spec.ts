2ba6966dbf83170d325f100c3c07ab05bbf2ab33
Simon Hofmann <simon.hofmann@consol.de>
2020-08-17 18:44:58 +0200

    (#390) Added test for getRegionFromFocusedWindow

--- packages/sakuli-legacy/src/context/common/environment/sakuli-environment.class.spec.ts
+++ packages/sakuli-legacy/src/context/common/environment/sakuli-environment.class.spec.ts
@@ -4,17 +4,23 @@ import { Project } from "@sakuli/core";
 import { LegacyProjectProperties } from "../../../loader/legacy-project-properties.class";
 import * as actions from "../actions";
 import { createEnvironmentClass } from "./sakuli-environment.class";
-import any = jasmine.any;
+import { ScreenApi } from "../actions";
+import { createRegionClass } from "../region";
 
 jest.mock("../actions");
 
 describe("sakuli environment", () => {
   const testExecutionContextMock = createTestExecutionContextMock();
+  const projectMock = mockPartial<Project>({
+    objectFactory: jest.fn().mockReturnValue(new LegacyProjectProperties()),
+  });
   const sakuliEnvironmentClass = createEnvironmentClass(
     testExecutionContextMock,
-    mockPartial<Project>({
-      objectFactory: jest.fn().mockReturnValue(new LegacyProjectProperties()),
-    })
+    projectMock
+  );
+  const sakuliRegionClass = createRegionClass(
+    testExecutionContextMock,
+    projectMock
   );
   const sakuliEnvironment = new sakuliEnvironmentClass();
 
@@ -35,7 +41,7 @@ describe("sakuli environment", () => {
     expect(runAsActionSpy).toBeCalledWith(
       testExecutionContextMock,
       "mouseWheelDown",
-      any(Function)
+      expect.any(Function)
     );
   });
 
@@ -52,7 +58,24 @@ describe("sakuli environment", () => {
     expect(runAsActionSpy).toBeCalledWith(
       testExecutionContextMock,
       "mouseWheelUp",
-      any(Function)
+      expect.any(Function)
+    );
+  });
+
+  it("should invoke ScreenApi on getRegionFromFocusedWindow", async () => {
+    //GIVEN
+    const expectedRegion = new sakuliRegionClass(10, 20, 30, 40);
+
+    //WHEN
+    const resultRegion = await sakuliEnvironment.getRegionFromFocusedWindow();
+
+    //THEN
+    expect(ScreenApi.getRegionFromFocusedWindow).toBeCalledTimes(1);
+    expect(resultRegion).toEqual(expectedRegion);
+    expect(runAsActionSpy).toBeCalledWith(
+      testExecutionContextMock,
+      "getRegionFromFocusedWindow",
+      expect.any(Function)
     );
   });
 });
