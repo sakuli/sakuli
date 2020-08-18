a45723bdbbea3926e60396eb4f807cd4e9d8b7cf
Simon Hofmann <simon.hofmann@consol.de>
2020-08-18 09:23:44 +0200

    (#390) Extended ScreenAPI mock with getRegionFromFocusedWindow

--- packages/sakuli-legacy/src/context/common/actions/__mocks__/screen.function.ts
+++ packages/sakuli-legacy/src/context/common/actions/__mocks__/screen.function.ts
@@ -1,12 +1,12 @@
 import { Region } from "../../region";
-import { SearchResult } from "../screen.function";
+import { ScreenRegionResult } from "../screen.function";
 
 const highlightMock = jest.fn(
   (regionToHighlight: Region) =>
     new Promise<Region>((res) => res(regionToHighlight))
 );
 const findMock = jest.fn(() => {
-  return new Promise<SearchResult>((resolve, reject) => {
+  return new Promise<ScreenRegionResult>((resolve, reject) => {
     resolve({
       left: 0,
       top: 0,
@@ -16,7 +16,7 @@ const findMock = jest.fn(() => {
   });
 });
 const waitForImageMock = jest.fn(() => {
-  return new Promise<SearchResult>((resolve, reject) => {
+  return new Promise<ScreenRegionResult>((resolve, reject) => {
     resolve({
       left: 0,
       top: 0,
@@ -29,6 +29,16 @@ const heightMock = jest.fn();
 const widthMock = jest.fn();
 const takeScreenShotMock = jest.fn();
 const takeScreenShotWithTimestampMock = jest.fn();
+const getRegionFromFocusedWindowMock = jest.fn(() => {
+  return new Promise<ScreenRegionResult>((resolve, reject) => {
+    resolve({
+      left: 10,
+      top: 20,
+      width: 30,
+      height: 40,
+    });
+  });
+});
 
 export const ScreenApi = {
   highlight: highlightMock,
@@ -38,4 +48,5 @@ export const ScreenApi = {
   width: widthMock,
   takeScreenshot: takeScreenShotMock,
   takeScreenshotWithTimestamp: takeScreenShotWithTimestampMock,
+  getRegionFromFocusedWindow: getRegionFromFocusedWindowMock,
 };
