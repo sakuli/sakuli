60c003c6890be66042c6547a342dc44e0b959f4a
Simon Hofmann <simon.hofmann@consol.de>
2020-08-25 12:20:11 +0200

    (#434) Added highlight to utils export

--- packages/sakuli-legacy/src/context/sahi/action/utils/index.ts
+++ packages/sakuli-legacy/src/context/sahi/action/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./create-with-action-context.function";
 export * from "./create-with-retries.function";
 export * from "./is-element-covered.function";
 export * from "./scroll-into-view-if-needed.function";
+export * from "./highlight.function";
