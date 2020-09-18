ec42f9fa611f4c41bc12bd338497bcc8ada20083
Van Cliff Le <cliff.le.van@consol.de>
2020-09-18 12:07:56 +0200

    (#419) run test:it before sonar-scanner

--- .build/run_linux.sh
+++ .build/run_linux.sh
@@ -6,9 +6,9 @@ echo $PWD
 npm ci
 npm run build
 FIREFOX_WD_URL=http://localhost:4444/wd/hub CHROME_WD_URL=http://localhost:4445/wd/hub npm test -- --runInBand --ci --bail
+npm run test:it
 if [[ $TRAVIS_NODE_VERSION = "lts/erbium" ]]; then
   sonar-scanner
 fi
-npm run test:it
 npm run test:e2e
 lerna run typedoc
