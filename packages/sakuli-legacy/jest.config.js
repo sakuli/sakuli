50bc316a2f5ce1c449493988e6ac5afb5659b649
Van Cliff Le <cliff.le.van@consol.de>
2020-09-18 13:14:32 +0200

    (#419) merged unit and it test coverage

--- packages/sakuli-legacy/jest.config.js
+++ packages/sakuli-legacy/jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   testEnvironment: "node",
   name: "sakuli-legacy",
   displayName: "sakuli-legacy",
-  coverageDirectory: "<rootDir>/../coverage/sakuli-legacy",
   rootDir: "src/",
   testMatch: [
     `**/!(*.+(aix|it).*|*.+(darwin|it).*|*.+(freebsd|it).*|*.+(linux|it).*|*.+(openbsd|it).*|*.+(sunos|it).*|*.+(win32|it).*)+(spec|test).[jt]s?(x)`,
