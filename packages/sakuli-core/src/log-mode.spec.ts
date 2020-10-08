e078ff7e2903674739adb68fd39cc4b3620c1692
Julian Liebich <Julian.Liebich@consol.de>
2020-10-08 13:44:25 +0200

    (#495) added testcase for parseLogMode

--- packages/sakuli-core/src/log-mode.spec.ts
+++ packages/sakuli-core/src/log-mode.spec.ts
@@ -1,8 +1,12 @@
 import { LogMode, parseLogMode } from "./log-mode";
 
 describe("log-mode", () => {
-  it("should return logMode corresponding to string input", () => {
+  it("should return logMode CI when string input 'ci'", () => {
     expect(parseLogMode("ci")).toBe(LogMode.CI);
+  });
+
+  it("should return logMode LOG_FILE when string input 'logfile", () => {
+    expect(parseLogMode("logfile")).toBe(LogMode.LOG_FILE);
   });
 
   it("should return undefined if string is not contained by enum", () => {
