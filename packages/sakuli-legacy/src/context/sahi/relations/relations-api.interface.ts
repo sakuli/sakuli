3e7c8320487ef81a35789d792d6bcc6a1f176864
Julian Liebich <Julian.Liebich@consol.de>
2020-12-02 18:16:13 +0100

    (sakuli/docs#32) Prototypical explanations for some of the api functions.

--- packages/sakuli-legacy/src/context/sahi/relations/relations-api.interface.ts
+++ packages/sakuli-legacy/src/context/sahi/relations/relations-api.interface.ts
@@ -3,10 +3,69 @@ import { SahiElementQueryOrWebElement } from "../sahi-element.interface";
 import { SahiRelation } from "./sahi-relation.interface";
 
 export interface RelationApi extends ParentApi {
+  /**
+   * Create a relation that specifies elements inside an anchor element.
+   *
+   * @example
+   * <br />Assuming this HTML-snippet:
+   *
+   * ```html
+   * <b>something</b>
+   * <div>
+   *   <b>something else</b>
+   * </div>
+   * ```
+   *
+   * The following expression ...
+   * ```typescript
+   * await _bold(0, _in(_div(1)))
+   * ```
+   * ... would return the second bold element.
+   * @param anchor
+   */
   _in(anchor: SahiElementQueryOrWebElement): SahiRelation;
   _near(anchor: SahiElementQueryOrWebElement): SahiRelation;
+
+  /**
+   * Create a relation that specifies elements that are geometrically under an anchor element.
+   *
+   * **Note**: The specified elements must be intersected by the same vertical line. E.g.:
+   *
+   * ```
+   * +----+
+   * | 1  |
+   * +----+
+   *          +----+
+   *          | 2  |
+   *          +----+
+   *      +----+
+   *      | 3  |
+   *      +----+
+   * ```
+   * In this sense element `3` is  "under" element `2` and element `3` is also "under" element `1`, but element `2` is not "under" element `1`.
+   * @param anchor
+   * @param offset
+   */
   _under(anchor: SahiElementQueryOrWebElement, offset?: number): SahiRelation;
+
+  /**
+   * Create a relation that specifies elements that are geometrically above an anchor element.
+   *
+   * **Note**: The specified elements must be intersected by the same vertical line.
+   * This works just the same as in [[_under]].
+   * @param anchor
+   * @param offset
+   */
   _above(anchor: SahiElementQueryOrWebElement, offset?: number): SahiRelation;
+
+  /**
+   * Create a relation that specifies elements that are geometrically under or above an anchor element.
+   *
+   * **Note**: The specified elements must be intersected by the same vertical line.
+   * This works just the same as in [[_under]].
+   * @param anchor
+   * @param offset
+   */
   _underOrAbove(
     anchor: SahiElementQueryOrWebElement,
     offset?: number
