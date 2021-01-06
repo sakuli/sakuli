4bd55f4f8f7498fe551711489de94de74edf2840
Van Cliff Le <cliff.le.van@consol.de>
2021-01-06 07:38:40 +0100

    (#576) replaced some steps with custom github actions

--- .build/trigger-image-build.sh
+++
@@ -1,4 +0,0 @@
-curl -H "Authorization: token $SAKULIDEV_TOKEN" \
-    --request POST \
-    --data '{"event_type": "build-latest"}' \
-    https://api.github.com/repos/sakuli/sakuli-docker/dispatches
